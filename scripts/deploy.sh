b31679a455df6e8c99efb2f1cb56d6df3a0446d9
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-19 17:05:47 +0900

    fix symbolic link path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -e
+ROOT_DIR=${HOME}/.local/share/chezmoi
 
 # symlink via GNU stow
-stow -v -d ${HOME}/.dotfiles/packages -t ~ fish git nvim bat bottom karabiner
+stow -v -d ${ROOT_DIR}/packages -t ~ fish
+stow -v -d ${ROOT_DIR}/packages -t ~ git
+stow -v -d ${ROOT_DIR}/packages -t ~ nvim
+stow -v -d ${ROOT_DIR}/packages -t ~ bat
+stow -v -d ${ROOT_DIR}/packages -t ~ bottom
