57d5881e87359b897304fbbfc4601acaac51842b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-20 20:54:28 +0900

    create install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# -*-mode:sh-*- vim:ft=sh
+
+set -e
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --verbose --apply git@github.com:MasahiroSakoda/dotfiles.git
