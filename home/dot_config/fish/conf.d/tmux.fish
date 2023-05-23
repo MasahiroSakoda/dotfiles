fad67ed4923b627ff7e2c3fede0f2e703e62e557
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-24 02:35:24 +0900

    configure tmux plugin manager

---
+++ home/dot_config/fish/conf.d/tmux.fish
@@ -0,0 +1,8 @@
+# -*-mode:fish-*- vim:ft=fish
+
+# ~/.local/share/tmux as plugin directory
+set -x TMUX_HOME "$XDG_CONFIG_HOME/tmux"
+set -x TMUX_PLUGIN_MANAGER_PATH "$XDG_DATA_HOME/tmux/tpm"
+
+set -g @plugin 'tmux-plugins/tpm'
+run "$TMUX_PLUGIN_MANAGER_PATH/tpm"
