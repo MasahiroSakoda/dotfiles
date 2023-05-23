f863def52f2c806b0d5bfbd3377aea0c76559dd1
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-24 03:39:40 +0900

    Fix tmux plugin directory

--- home/dot_config/fish/conf.d/tmux.fish
+++ home/dot_config/fish/conf.d/tmux.fish
@@ -2,4 +2,4 @@
 
 # ~/.local/share/tmux as plugin directory
 set -x TMUX_HOME "$XDG_CONFIG_HOME/tmux"
-set -x TMUX_PLUGIN_MANAGER_PATH "$XDG_DATA_HOME/tmux/tpm"
+set -x TMUX_PLUGIN_MANAGER_PATH "$XDG_DATA_HOME/tmux/plugins"
