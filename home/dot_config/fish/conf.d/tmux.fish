3712f61068c12f0953c924d813ba27e2b1be4fca
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-24 03:28:55 +0900

    remove wrong config for tmux

--- home/dot_config/fish/conf.d/tmux.fish
+++ home/dot_config/fish/conf.d/tmux.fish
@@ -3,6 +3,3 @@
 # ~/.local/share/tmux as plugin directory
 set -x TMUX_HOME "$XDG_CONFIG_HOME/tmux"
 set -x TMUX_PLUGIN_MANAGER_PATH "$XDG_DATA_HOME/tmux/tpm"
-
-set -g @plugin 'tmux-plugins/tpm'
-run "$TMUX_PLUGIN_MANAGER_PATH/tpm"
