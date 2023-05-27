238e4ae9444fa14363263f5e541184fe311a1e97
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-05-27 19:06:37 +0900

    replace SF Symbols with nerd font symbols

--- home/dot_config/nvim/lua/dap/config/ui.lua
+++ home/dot_config/nvim/lua/dap/config/ui.lua
@@ -42,12 +42,12 @@ dapui.setup({
     icons = {
       pause     = "",
       play      = "",
-      step_into = "",
-      step_over = "",
-      step_out  = "",
+      step_into = "",
+      step_over = "",
+      step_out  = "",
       step_back = "",
       run_last  = "↻",
-      terminate = "□",
+      terminate = "",
     },
   },
 })
