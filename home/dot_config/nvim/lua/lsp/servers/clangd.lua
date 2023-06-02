cab584bae1557bfdfa4a32d2b56b79ebf23a93b8
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-06-02 14:14:13 +0900

    update `clangd` options

--- home/dot_config/nvim/lua/lsp/servers/clangd.lua
+++ home/dot_config/nvim/lua/lsp/servers/clangd.lua
@@ -4,18 +4,18 @@ if not ok then return end
 return {
   cmd       = {
     "clangd",
-    "--offset-encoding=utf-16", -- for 'multiple client offset encoding' error
-    "--all-scopes-completion",
-    "--background-index",
-    "--pch-storage=disk",
-    "--log=verbose",
-    "--completion-style=detailed",
-    "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
     "--clang-tidy",
-    "--query-driver=/home/linuxbrew/.linuxbrew/Cellar/gcc/11.3.0_2",
-    "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
-    -- "--fallback-style=Google",
-    -- "--compile-commands-dir=build",
+    "--log=verbose",
+    "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+    "--background-index",
+    "--pch-storage=memory",
+    "--all-scopes-completion",
+    "--completion-style=detailed",
+    "--suggest-missing-includes",
+    "--cross-file-rename",
+    "--folding-ranges",
+    "--fallback-style=llvm",
+    "--offset-encoding=utf-16", -- for 'multiple client offset encoding' error
   },
   filetypes = {
     "c", "h",
