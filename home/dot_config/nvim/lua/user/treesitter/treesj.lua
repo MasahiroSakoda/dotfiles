cff33d304e07c51bc0b6ad8cab0dee250372e426
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-07-06 02:20:38 +0900

    add `treesj` as NeoVim plugin

---
+++ home/dot_config/nvim/lua/user/treesitter/treesj.lua
@@ -0,0 +1,19 @@
+local ok, treesj = pcall(require, "treesj")
+if not ok then return end
+
+-- https://github.com/Wansmer/treesj#settings
+treesj.setup({
+  -- Use default keymaps
+  -- (<space>m - toggle, <space>j - join, <space>s - split)
+  use_default_keymaps = false,
+
+  -- Node with syntax error will not be formatted
+  check_syntax_error = true,
+
+-- If line after join will be longer than max value,
+  -- node will not be formatted
+  max_join_length = 120,
+
+  -- Use `dot` for repeat action
+  dot_repeat = true,
+})
