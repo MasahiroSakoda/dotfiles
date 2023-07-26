3072b65a01100b62a200703daa4edee54a70f9e2
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-07-26 17:57:40 +0900

    add config for `rubocop` with builtin LSP

---
+++ home/dot_config/nvim/lua/lsp/servers/rubocop.lua
@@ -0,0 +1,14 @@
+local ok, lspconfig = pcall(require, "lspconfig")
+if not ok then return end
+
+return {
+  cmd       = { "bundle", "exec", "rubocop", "-lsp" },
+  filetypes = { "ruby" },
+  root_dir  = function(filename)
+    return lspconfig.util.root_pattern(
+      "Gemfile",
+      ".git"
+    )(filename) or vim.fn.getcwd()
+  end,
+  single_file_support = true,
+}
