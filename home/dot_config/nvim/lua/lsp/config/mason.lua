7b674b554889b795c7dbd23ec383e6cd856203b4
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-09-30 06:56:00 +0900

    update `mason.nvim` config

--- home/dot_config/nvim/lua/lsp/config/mason.lua
+++ home/dot_config/nvim/lua/lsp/config/mason.lua
@@ -2,11 +2,26 @@ local ok, mason = pcall(require, "mason")
 if not ok then return end
 
 mason.setup({
+  log_level = vim.log.levels.DEBUG,
+  max_concurrent_installers = 4,
+  check_outdated_packages_on_open = true,
+
   ui = {
-    icons = {
+    width  = 0.7,
+    height = 0.85,
+    icons  = {
       package_pending     = "➜",
       package_installed   = "✓",
       package_uninstalled = "✗"
+    },
+    keymaps = {
+      toggle_package_expand   = "<CR>",
+      install_package         = "i",
+      update_package          = "u",
+      update_all_packages     = "U",
+      check_package           = "c",
+      check_outdated_packages = "C",
+      uninstall_package       = "X",
     }
-  }
+  },
 })
