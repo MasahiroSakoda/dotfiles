3a344c98f4f44fb45c3e0e73b264a58ad688aff0
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-10 08:57:39 +0900

    fix: update neovim dap config

--- home/dot_config/nvim/lua/dap/adapters/init.lua
+++ home/dot_config/nvim/lua/dap/adapters/init.lua
@@ -1,9 +1,9 @@
 return {
   bash     = {},                                 -- bash-debug-adapter
   codelldb = require("dap.adapters.codelldb"),   -- codelldb: C, C++, Rust
-  -- cppdbg   = require("dap.adapters.cppdbg"),     -- cpptools: C, C++, Rust
+  cpptools = require("dap.adapters.cpptools"),   -- cpptools: C, C++, Rust
   delve    = require("dap.adapters.delve"),      -- Go
-  python   = require("dap.adapters.python"),     -- Python
+  debugpy  = require("dap.adapters.debugpy"),    -- Python
   js       = require("dap.adapters.javascript"), -- JavaScript, TypeScript
   chrome   = require("dap.adapters.chrome"),     -- Google Chrome
 }
