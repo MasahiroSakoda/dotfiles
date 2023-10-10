80dfb00dfd01c31cf47cd1ac0af758da6472fc1a
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-10 17:08:33 +0900

    feat(eza): configure time style

--- home/dot_config/fish/functions/ll.fish
+++ home/dot_config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll
-  command eza -al --icons --header --color=automatic --group-directories-first --git-ignore $argv
+  command eza -al --icons --header --time-style=iso --color=automatic --group-directories-first --git-ignore $argv
 end
