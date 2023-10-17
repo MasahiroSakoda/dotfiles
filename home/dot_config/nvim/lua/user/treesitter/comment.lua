12c775c43001170908ed40899600ef36ff815841
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-18 02:55:18 +0900

    refactor(neovim): add ignore config for `Comment.nvim`

--- home/dot_config/nvim/lua/user/treesitter/comment.lua
+++ home/dot_config/nvim/lua/user/treesitter/comment.lua
@@ -3,4 +3,7 @@ if not ok then return end
 
 comment.setup({
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+
+  -- Ignore empty lines
+  ignore = "^$",
 })
