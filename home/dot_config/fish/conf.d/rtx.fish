06e83b52e2521b129460dda715d9e11931bc7297
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-21 01:36:00 +0900

    feat(fish): add `rtx` environment variables

--- home/dot_config/fish/conf.d/rtx.fish
+++ home/dot_config/fish/conf.d/rtx.fish
@@ -4,3 +4,6 @@ set -gx RTX_DATA_DIR "$XDG_DATA_HOME/rtx"
 set -gx RTX_CONFIG_FILE "$XDG_CONFIG_HOME/rtx/config.toml"
 
 type -q rtx && rtx activate fish | source && rtx completion fish | source
+
+set -gx RTX_LOG_LEVEL info # Usage: trace|debug|info|warn|error
+set -gx RTX_FETCH_REMOTE_VERSIONS_TIMEOUT "1800 s"
