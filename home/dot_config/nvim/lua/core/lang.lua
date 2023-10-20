e0561b13c347c7fc849d84d3fdd4ca6f7f8ab3e9
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-21 01:13:36 +0900

    fix(neovim): update config for specific language

--- home/dot_config/nvim/lua/core/lang.lua
+++ home/dot_config/nvim/lua/core/lang.lua
@@ -1,17 +1,39 @@
-local fn, g = vim.fn, vim.g
+local g, fn = vim.g, vim.fn
 
--- Configure Python environment
-if fn.exists("$VIRTURL_ENV") == 1 then
-  g.python3_host_prog = fn.substitute(fn.system("which -a python3 | head -n2 | tail -n1"), "\n", "", "g")
-  local is_pynvim_installed = string.len(fn.system("pip freeze | grep pynvim")) ~= 0
-  local is_pyright_installed = string.len(fn.system("pip freeze | grep pyright")) ~= 0
-  if not is_pynvim_installed and not is_pyright_installed then
-    print("pyright and/or pynvim not detected. Installing...")
-    vim.api.nvim_exec("!pip install pynvim pyright", false)
-  end
-else
-  g.python3_host_prog = fn.substitute(fn.system("which python3"), "\n", "", "g")
-end
+--------------------------------------------------
+-- Python environment
+--------------------------------------------------
+g.loaded_python_provider  = 0 -- disable Python 2.x provider
+g.loaded_python3_provider = 1
 
--- Configure Ruby environment
--- g.ruby_host
+-- Python This must be here becasue it makes loading vim VERY SLOW otherwise
+g.python_host_skip_check  = 1
+g.python3_host_skip_check = 1
+
+g.python3_host_prog = fn.exepath("neovim-python-host")
+
+-- TODO: Configure `python3_host_prog` for Poetry virtual env
+-- if vim.fn.exists("$VIRTURL_ENV") then
+--   g.python3_host_prog = fn.substitute(fn.system("which -a python3 | head -n2 | tail -n1"), "\n", "", "g")
+-- else
+--   g.python3_host_prog = fn.substitute(fn.system("which python3"), "\n", "", "g")
+-- end
+
+--------------------------------------------------
+-- Ruby environment
+--------------------------------------------------
+g.loaded_ruby_provider = 1
+g.ruby_host_skip_check = 1
+g.ruby_host_prog       = fn.exepath("neovim-ruby-host")
+
+--------------------------------------------------
+-- Perl environment
+--------------------------------------------------
+g.loaded_perl_provider = 0
+g.perl_host_skip_check = 1
+
+--------------------------------------------------
+-- Node.js environment
+--------------------------------------------------
+g.loaded_node_provider = 1
+g.node_host_prog = fn.exepath("neovim-node-host")
