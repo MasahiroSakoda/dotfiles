64dc3261f1b9eda8ed3ed0ab1efa2aefe9444d99
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-21 15:45:39 +0900

    fix(python/ruby): update `configure` options

--- home/dot_config/fish/conf.d/lang.fish
+++ home/dot_config/fish/conf.d/lang.fish
@@ -3,9 +3,12 @@
 # Rust
 set -gx RUST_BACKTRACE full
 
+# Python
+set -gx PYTHON_CONFIGURE_OPTS "--with-openssl-dir='$(brew --prefix openssl@3)' --with-readline-dir='$(brew --prefix readline)' --with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'"
+
 # Ruby
 set -gx RUBY_CFLAGS "-Wno-error=implicit-function-declaration"
-set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@3)"
+set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@3) --with-readline-dir=$(brew --prefix readline)"
 
 # Node.js
 set -gx NODE_ENV development
