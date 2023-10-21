07621d64709b7a274be54dcb664ac8b7a0b2877c
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-10-21 15:47:13 +0900

    refactor(compiler): refine compiler flags

--- home/dot_config/fish/conf.d/flag.fish
+++ home/dot_config/fish/conf.d/flag.fish
@@ -1,25 +1,7 @@
 # -*-mode:fish-*- vim:ft=fish
 
-# GNU binutils
-set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/binutils/lib"
-set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/binutils/include"
-
-# zlib
-set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/zlib/lib"
-set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/zlib/include"
-set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/zlib/lib/pkgconfig"
-
-# Readline
-set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/readline/lib"
-set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/readline/opt/readline/include"
-set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/readline/lib/pkgconfig"
-
-# cURL
-set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/curl/lib"
-set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/curl/include"
-set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/curl/lib/pkgconfig"
-
-# libffi
-set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/libffi/lib"
-set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/libffi/include"
-set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/libffi/lib/pkgconfig"
+for pkg in binutils zlib readline openssl@3 curl libffi tcl-tk
+  set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/$pkg/lib" $LDFLAGS
+  set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CPPFLAGS
+  set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$pkg/lib/pkgcofig" $PKG_CONFIG_PATH
+end
