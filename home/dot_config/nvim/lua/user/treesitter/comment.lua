6762824993a10bf4247b6040ff589359695e74be
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-20 00:52:37 +0900

    feat(neovim): udpate `nvim-ts-context-commentstring` config
    integrated with `numToStr/Comment.nvim`

--- home/dot_config/nvim/lua/user/treesitter/comment.lua
+++ home/dot_config/nvim/lua/user/treesitter/comment.lua
@@ -1,6 +1,9 @@
 local ok, comment = pcall(require, "Comment")
 if not ok then return end
 
+-- to skip backwards compatibility routines and speed up loading
+vim.g.skip_ts_context_commentstring_module = true
+
 comment.setup({
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 
