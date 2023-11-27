b28720341aacfb68ab1ba6b5a145ccba84114d2e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-27 11:08:37 +0900

    feat(neovim): update golang LSP & linter config
    disable diagnostics to use `revive`

--- home/dot_config/nvim/lua/lsp/servers/gopls.lua
+++ home/dot_config/nvim/lua/lsp/servers/gopls.lua
@@ -7,15 +7,21 @@ return {
   root_dir  = lspconfig.util.root_pattern("go.mod", "go.work", ".git"),
   settings  = {
     gopls = {
+      -- Completion settings
       usePlaceholders = true,
       completeUnimported = true,
       experimentalPostfixCompletions = true,
-      staticcheck = true,
-      analyses = {
-        unusedparams = true,
-        shadow = false,
-      }
-    }
+      completionDocumentation = true,
+      deepCompletion = true,
+
+      -- the following diagnostics options are leave it to `revive`
+      -- staticcheck = true,
+      -- analyses = {
+      --   unusedparams = true,
+      --   unreachable  = true,
+      --   shadow       = false,
+      -- },
+    },
   },
   single_file_support = true,
 }
