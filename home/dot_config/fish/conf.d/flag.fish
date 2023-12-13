5e77698fd41aac70227a9dede972f4f79f55a669
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-12-13 20:17:48 +0900

    refactor(fish): refactor compiler flag configurations

--- home/dot_config/fish/conf.d/flag.fish
+++ home/dot_config/fish/conf.d/flag.fish
@@ -1,6 +1,6 @@
 # -*-mode:fish-*- vim:ft=fish
 
-for pkg in binutils zlib readline openssl@3 curl libffi tcl-tk
+for pkg in ncurses sqlite curl
   set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/$pkg/lib" $LDFLAGS
   set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CPPFLAGS
   set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$pkg/lib/pkgcofig" $PKG_CONFIG_PATH
