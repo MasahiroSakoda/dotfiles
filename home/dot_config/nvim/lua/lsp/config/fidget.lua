1cbb4e6ef426604651de249d86e303d7251ac961
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-12-17 22:08:17 +0900

    feat(neovim): update `fidget.nvim` config

--- home/dot_config/nvim/lua/lsp/config/fidget.lua
+++ home/dot_config/nvim/lua/lsp/config/fidget.lua
@@ -2,20 +2,37 @@ local ok, fidget = pcall(require, "fidget")
 if not ok then return end
 
 fidget.setup({
-  window = {
-    blend    = 35,
-    relative = "win",
+
+  progress = {
+    suppress_on_insert   = false,
+    ignore_done_already  = true,
+    ignore_empty_message = true,
+    ignore = { -- List of LSP Server to ignore
+    },
+
+    display = {
+      progress_icon = {
+        { pattern = "grow_vertical", period = 1 },
+      },
+      overrides = {
+        rust_analyzer = { name = "rust-analyzer" },
+      },
+    },
   },
-  text = {
-    -- Available spinner: earth, moon, grow_vertical, meter, dots_pulse
-    spinner = "dots_pulse",
-  },
-  timer = {
-    fidget_decay = 2500,
-    task_decay   = 1500,
-  },
-  align = {
-    bottom = true,
-    right  = true,
+
+  notification = {
+    view = {
+      group_separator = "----------",
+    },
+    window = {
+      winblend   = 35,
+      border     = "none", --- @type "none"|"single"|"double"|"rounded"|"solid"|"shadow"
+      max_width  = 0,
+      max_height = 0,
+      x_padding  = 1,
+      y_padding  = 0,
+      align    = "bottom", --- @type "top"|"bottom"
+      relative = "editor", --- @type "editor"|"win"
+    },
   },
 })
