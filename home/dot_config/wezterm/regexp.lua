e8b87a91682ea0962c0b6df6ba4a9ef490fed973
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-01-02 10:22:16 +0900

    feat(wezterm): define regexp for quick select & hyperlink

---
+++ home/dot_config/wezterm/regexp.lua
@@ -0,0 +1,18 @@
+-- -*-mode:lua-*- vim:ft=lua
+
+return {
+  pattern = {
+    line    = "^.+$",
+    ip      = "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+",
+    url     = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+    http    = "https?://\\S+\\b",
+    git     = "git://\\S+\\b",
+    ssh     = "ssh://\\S+\\b",
+    ftp     = "ftp://\\S+\\b",
+    file    = "file://\\S+\\b",
+    mailto  = "mailto://\\S+\\b",
+    email   = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+    hash    = "(sha256-[0-9a-zA-z=/+]{44}|[0-9a-f]{7,40}|[0-9a-z]{52})",
+    version = "v?\\d+(?:\\.\\d+)+",
+  },
+}
