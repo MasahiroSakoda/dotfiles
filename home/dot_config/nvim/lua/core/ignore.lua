47ad683056640257620b44c3fce5fc381d61d147
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-01-09 15:28:31 +0900

    feat(neovim): add fuzzy finder ignore patterns

---
+++ home/dot_config/nvim/lua/core/ignore.lua
@@ -0,0 +1,30 @@
+-- -*-mode:lua-*- vim:ft=lua
+
+return {
+  "%.tar", "%.tar.gz", "%.tar.xz", "%.zip", "%.rar", "%.7z", "%.bz2",
+  "^build/", "^dist/", "^.cache/", "%.o", "%.out", "%.a", "%.so",
+  "^.git/", "^.svn/", "^.hg/",
+
+  ".DS_Store", "%.dylib", "^.Trashes/",
+  "%.exe", "Thumb.db", "desktop.ini",
+  ".Trash-*", "^.directory",
+
+  "^.idea/", "^.vscode/", "^.project",
+  "^debug/", "^target/",
+  "^.gradle/", "^.classpath/", "^.factorypath/", "%.class", "%.jar", "%.war",
+  "%.dll", "%.pdb",
+  "^node_modules/", "^.yarn/", "^.pnpm/", "^.npm/", "package-lock.json",
+  "%.min.js", "%.min.gzip.js",
+  "^.venv/", "%.pyc", "^__pycache__/", "%.ipynb",
+  "^vendor/", "^deps/",
+  "%.sqlite", "%.sqlite3", "%.db",
+  "%.lock",
+
+  "%.ai", "%.psd",
+  "%.otf", "%.ttf", "%.woff",
+  "%.ico", "%.svg",
+  "%.png", "%.jpg", "%.jpeg", "%.webp", "%.gif",
+  "%.mp4", "%.mkv", "%.m4v", "%.avi", "%.flv",
+  "%.wav", "%.flac", "%.ttf", "%.mp3", "%.m4a", "%.ogg",
+  "%.doc", "%.xls", "%.ppt", "%.pdf", "%.epub",
+}
