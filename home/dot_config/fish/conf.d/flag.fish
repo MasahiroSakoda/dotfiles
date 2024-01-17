80b9f48195eec8799a693567dc2c606076cfcef6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-01-17 16:17:09 +0900

    fix(fish): update environment variables to build `python`

--- home/dot_config/fish/conf.d/flag.fish
+++ home/dot_config/fish/conf.d/flag.fish
@@ -1,7 +1,8 @@
 # -*-mode:fish-*- vim:ft=fish
 
-for pkg in ncurses sqlite curl
-  set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/$pkg/lib" $LDFLAGS
+for pkg in openssl curl readline gettext ncurses sqlite zlib tcl-tk
+  set -gx LDFLAGS  "-L$HOMEBREW_PREFIX/opt/$pkg/lib" $LDFLAGS
+  set -gx CFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CFLAGS
   set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CPPFLAGS
-  set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$pkg/lib/pkgcofig" $PKG_CONFIG_PATH
+  set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$pkg/lib/pkgconfig" $PKG_CONFIG_PATH
 end
