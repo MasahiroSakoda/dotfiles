e0d5e34aefea5151087fb72f610cd863aad597e2
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-01-18 23:32:47 +0900

    fix(yt-dlp): fix `shellcheck` errors

--- home/dot_local/bin/private_executable_download_audio.sh
+++ home/dot_local/bin/private_executable_download_audio.sh
@@ -7,5 +7,5 @@ DOWNLOAD_PATH="$HOME/Downloads/YouTube/Audio"
 DOWNLOADER_OPTS="--external-downloader aria2c --external-downloader-args '-c -x 5 -k 2M'"
 AUDIO_OPTS="--extract-audio --audio-format mp3 --audio-quiality 128k"
 
-cd $DOWNLOAD_PATH
-$(command -v yt-dlp) -civ $DOWNLOADER_OPTS $AUDIO_OPTS -o "%(title)s.%(ext)s" $1
+cd "$DOWNLOAD_PATH"
+$(command -v yt-dlp) -civ "$DOWNLOADER_OPTS" "$AUDIO_OPTS" -o "%(title)s.%(ext)s" $1
--- home/dot_local/bin/private_executable_download_video.sh
+++ home/dot_local/bin/private_executable_download_video.sh
@@ -8,5 +8,5 @@ DOWNLOADER_OPTS="--external-downloader aria2c --external-downloader-args '-c -x 
 FORMAT_OPTS="bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 CODEC_OPTS="vcodec:h264"
 
-cd $DOWNLOAD_PATH
-$(command -v yt-dlp) -civ $DOWNLOADER_OPTS -f $FORMAT_OPTS -S $CODEC_OPTS -o "%(title)s.%(ext)s" $1
+cd "$DOWNLOAD_PATH"
+$(command -v yt-dlp) -civ "$DOWNLOADER_OPTS" -f "$FORMAT_OPTS" -S "$CODEC_OPTS" -o "%(title)s.%(ext)s" $1
