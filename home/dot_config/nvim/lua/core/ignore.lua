5cdc7b24937227d5879898c22581739cb928abcf
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-01-23 16:48:10 +0900

    feat(neovim): specify extension with reexp to ignore for telescope fuzzy search

--- home/dot_config/nvim/lua/core/ignore.lua
+++ home/dot_config/nvim/lua/core/ignore.lua
@@ -1,8 +1,8 @@
 -- -*-mode:lua-*- vim:ft=lua
 
 return {
-  "%.tar", "%.tar.gz", "%.tar.xz", "%.zip", "%.rar", "%.7z", "%.bz2",
-  "^build/", "^dist/", "^.cache/", "%.o", "%.out", "%.a", "%.so",
+  "%.tar$", "%.tar.gz$", "%.tar.xz$", "%.zip$", "%.rar$", "%.7z$", "%.bz2$",
+  "^build/", "^dist/", "^.cache/", "%.o$", "%.out$", "%.a$", "%.so$", "%.whl$", "%.bin$",
   "^.git/", "^.svn/", "^.hg/",
 
   ".DS_Store", "%.dylib", "^.Trashes/",
@@ -11,20 +11,20 @@ return {
 
   "^.idea/", "^.vscode/", "^.project",
   "^debug/", "^target/",
-  "^.gradle/", "^.classpath/", "^.factorypath/", "%.class", "%.jar", "%.war",
-  "%.dll", "%.pdb",
+  "^.gradle/", "^.classpath/", "^.factorypath/", "%.class$", "%.jar$", "%.war$",
+  "%.dll$", "%.pdb$",
   "^node_modules/", "^.yarn/", "^.pnpm/", "^.npm/", "package-lock.json",
-  "%.min.js", "%.min.gzip.js",
-  "^.venv/", "%.pyc", "^__pycache__/", "%.ipynb",
+  "%.min.js$", "%.min.gzip.js$",
+  "^.venv/", "%.pyc$", "^__pycache__/", "%.ipynb$",
   "^vendor/", "^deps/",
-  "%.sqlite", "%.sqlite3", "%.db",
-  "%.lock",
+  "%.sqlite$", "%.sqlite3$", "%.db$",
+  "%.lock$",
 
-  "%.ai", "%.psd",
-  "%.otf", "%.ttf", "%.woff",
-  "%.ico", "%.svg",
-  "%.png", "%.jpg", "%.jpeg", "%.webp", "%.gif",
-  "%.mp4", "%.mkv", "%.m4v", "%.avi", "%.flv",
-  "%.wav", "%.flac", "%.ttf", "%.mp3", "%.m4a", "%.ogg",
-  "%.doc", "%.xls", "%.ppt", "%.pdf", "%.epub",
+  "%.ai$", "%.psd$",
+  "%.otf$", "%.ttf$", "%.woff$",
+  "%.ico$", "%.svg$",
+  "%.png$", "%.jpg$", "%.jpeg$", "%.webp$", "%.gif$",
+  "%.mp4$", "%.mkv$", "%.m4v$", "%.avi$", "%.flv$",
+  "%.wav$", "%.flac$", "%.ttf$", "%.mp3$", "%.m4a$", "%.ogg$",
+  "%.doc$", "%.xls$", "%.ppt$", "%.pdf$", "%.epub$",
 }
