70128931d9ef983c884fb30df47ef5fcde183107
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-02-06 14:49:25 +0900

    feat(neovim): configure external plugin path installed by luarocks

---
+++ home/dot_config/nvim/lua/core/external.lua
@@ -0,0 +1,2 @@
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
