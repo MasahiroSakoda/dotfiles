fdbaa5f170349628019f4308044f30d58389a25a
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-02-08 19:57:09 +0900

    refactor(wezterm): fix leader key
    to prevent conflicting with neovim

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -95,7 +95,7 @@ config.disable_default_key_bindings = true   ---@type boolean
 
 config.keys       = keybinds.keys
 config.key_tables = keybinds.key_tables
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2500 }
+config.leader = { key = ";", mods = "CTRL", timeout_milliseconds = 2500 }
 
 --------------------------------------------------
 -- Hyperlink / Quick Select
