7fc94a1694f32c650cdfc3b17f3726907f7d1726
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-02-18 00:54:10 +0900

    feat(neovim): activate `command_palette` preset

--- home/dot_config/nvim/lua/ui/noice.lua
+++ home/dot_config/nvim/lua/ui/noice.lua
@@ -3,13 +3,13 @@ if not ok then return end
 
 noice.setup({
   -- you can enable a preset for easier configuration
-  -- presets = {
-  --   bottom_search         = false, -- use a classic bottom cmdline for search
-  --   command_palette       = false, -- position the cmdline and popupmenu together
-  --   long_message_to_split = false, -- long messages will be sent to a split
-  --   inc_rename            = false, -- enables an input dialog for inc-rename.nvim
-  --   lsp_doc_border        = false, -- add a border to hover docs and signature help
-  -- },
+  presets = {
+    bottom_search         = false, -- use a classic bottom cmdline for search
+    command_palette       = true,  -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename            = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border        = false, -- add a border to hover docs and signature help
+  },
 
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
