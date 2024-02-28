41c7d39510f2863809f86fefa57439028acb6249
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-02-28 12:18:51 +0900

    feat(fish): update compiler flag path
    `LDFLAGS`, `CFLAGS`& `CPPFLAGS`

--- home/dot_config/fish/conf.d/flag.fish
+++
@@ -1,8 +0,0 @@
-# -*-mode:fish-*- vim:ft=fish
-
-for pkg in openssl curl readline gettext ncurses sqlite zlib tcl-tk
-  set -gx LDFLAGS  "-L$HOMEBREW_PREFIX/opt/$pkg/lib" $LDFLAGS
-  set -gx CFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CFLAGS
-  set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/$pkg/include" $CPPFLAGS
-  set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$pkg/lib/pkgconfig" $PKG_CONFIG_PATH
-end
