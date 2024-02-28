9f61db89cd15e187cd4de4d438cc47742d16fd4f
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-02-28 15:47:40 +0900

    feat(neovim): add `telescope-live-grep-args` config file

---
+++ home/dot_config/nvim/lua/user/telescope/lga.lua
@@ -0,0 +1,15 @@
+local telescope_ok, telescope = pcall(require, "telescope")
+local actions_ok, actions     = pcall(require, "telescope-live-grep-args.actions")
+if not (telescope_ok or actions_ok) then return end
+
+telescope.setup({
+  extensions = {
+    live_grep_args = {
+      auto_quoting = true,
+      mappings = {
+        ["<C-k>"] = actions.quote_prompt(),
+        ["<C-i>"] = actions.quote_prompt({ postfix = " --iglob " }),
+      },
+    },
+  },
+})
