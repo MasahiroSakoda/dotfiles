539ee627b05b3c8597af7f2120156486ed720951
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-03-02 22:45:52 +0900

    feat(neovim): activate `bashls` `autostart` option

--- home/dot_config/nvim/lua/lsp/servers/bashls.lua
+++ home/dot_config/nvim/lua/lsp/servers/bashls.lua
@@ -5,5 +5,6 @@ return {
   cmd       = { "bash-language-server", "start" },
   cmd_env   = { GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)" },
   filetypes = { "sh" },
-  root_dir  = lspconfig.util.root_pattern(".bashrc")
+  root_dir  = lspconfig.util.root_pattern(".bashrc"),
+  autostart = true,
 }
