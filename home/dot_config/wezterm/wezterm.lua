94982eb84966d8ed45191ce4983e83e6d94e776d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-04-23 18:42:56 +0900

    chore(wezterm): update font size for command palette

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -109,7 +109,7 @@ config.quick_select_patterns = select.patterns
 --------------------------------------------------
 -- Command Palette
 --------------------------------------------------
-config.command_palette_font_size = 12
+config.command_palette_font_size = fonts.global.size
 config.command_palette_fg_color  = "#cdd6f4"
 config.command_palette_bg_color  = "#262626"
 
