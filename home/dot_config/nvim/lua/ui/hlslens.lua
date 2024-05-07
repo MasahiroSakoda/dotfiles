b7f01dbe6aa025455d67dfd1edc572882c64594e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-08 02:52:49 +0900

    chore(neovim): disable `nvim-scrollbar` integration for `nvim-hlslens`

--- home/dot_config/nvim/lua/ui/hlslens.lua
+++ home/dot_config/nvim/lua/ui/hlslens.lua
@@ -1,15 +1,15 @@
 local hlslens_ok, hlslens     = pcall(require, "hlslens")
-local searchbar_ok, searchbar = pcall(require, "scrollbar.handlers.search")
-if not (hlslens_ok or searchbar_ok) then return end
+-- local searchbar_ok, searchbar = pcall(require, "scrollbar.handlers.search")
+if not (hlslens_ok) then return end
 
 hlslens.setup({
   calm_down = true,
   nearest_only = true,
   -- nearest_float_when = 'always',
 
-  build_position_cb = function(plist, _, _, _)
-    searchbar.handler.show(plist.start_pos)
-  end,
+  -- build_position_cb = function(plist, _, _, _)
+  --   searchbar.handler.show(plist.start_pos)
+  -- end,
 
   -- Activate Scrollbar search
   -- searchbar.setup({
@@ -28,9 +28,9 @@ keymap("n", "#",  [[#<CMD>lua require("hlslens").start()<CR>]], opts)
 keymap("n", "g*", [[g*<CMD>lua require("hlslens").start()<CR>]], opts)
 keymap("n", "g#", [[g#<CMD>lua require("hlslens").start()<CR>]], opts)
 
-vim.cmd [[
-  augroup scrollbar_search_hide
-    autocmd!
-    autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-  augroup END
-]]
+-- vim.cmd [[
+--   augroup scrollbar_search_hide
+--     autocmd!
+--     autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+--   augroup END
+-- ]]
