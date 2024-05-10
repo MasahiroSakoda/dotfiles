973d56c19d911757184b2d3de9ceca9c2326f50b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-10 17:23:36 +0900

    chore: fix too long line to pass lint

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -14,7 +14,11 @@ return {
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    build  = [[
+      cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && \
+      cmake --build build --config Release && \
+      cmake --install build --prefix build
+    ]],
     cond   = not vim.g.vscode,
   },
   {
