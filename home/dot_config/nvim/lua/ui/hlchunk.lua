82cc12b43fbfb250a12d9712f0ac0c81ee4e8439
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-11 21:40:59 +0900

    revert(neovim): fix character to display chunk

--- home/dot_config/nvim/lua/ui/hlchunk.lua
+++ home/dot_config/nvim/lua/ui/hlchunk.lua
@@ -10,7 +10,7 @@ hlchunk.setup({
       horizontal_line = "─",
       vertical_line   = "│",
       left_top    = "┌",
-      left_bottom = "└>",
+      left_bottom = "└",
       right_arrow = "─",
     },
     style = "#00ffff",
