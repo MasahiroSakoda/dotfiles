026135c683b894f53b11e93e9843de23f6f5aa9d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-15 10:58:55 +0900

    fix: suppress warning

--- home/dot_config/nvim/lua/lsp/config/init.lua
+++ home/dot_config/nvim/lua/lsp/config/init.lua
@@ -34,18 +34,18 @@ capabilities.textDocument.foldingRange  = {
 }
 
 local active_clients = lsp.get_active_clients()
-local inlay_hint     = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+-- local inlay_hint     = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
 
-local setup_inlay_hints = function()
-  local hl = api.nvim_get_hl(0, { name = "Comment" })
-  local foreground = string.format("#%06x", hl["fg"] or 0)
-  if #foreground < 3 then foreground = "" end
+-- -- local setup_inlay_hints = function()
+-- --   local hl = api.nvim_get_hl(0, { name = "Comment" })
+-- --   local foreground = string.format("#%06x", hl["fg"] or 0)
+-- --   if #foreground < 3 then foreground = "" end
 
-  hl = api.nvim_get_hl(0, { name = "CursorLine" })
-  local background = string.format("#%06x", hl["bg"] or 0)
-  if #background < 3 then background = "" end
-  api.nvim_set_hl(0, 'LspInlayHint', { fg = foreground, bg = background })
-end
+-- --   hl = api.nvim_get_hl(0, { name = "CursorLine" })
+-- --   local background = string.format("#%06x", hl["bg"] or 0)
+-- --   if #background < 3 then background = "" end
+--   api.nvim_set_hl(0, 'LspInlayHint', { fg = foreground, bg = background })
+-- end
 
 local on_attach = function(client, bufnr)
   api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
