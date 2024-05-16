dd95a48a7379185967aa96ca41b84e7814bbbe86
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-16 19:20:36 +0900

    chore(neovim): separate lsp capabilities config

--- home/dot_config/nvim/lua/lsp/config/init.lua
+++ home/dot_config/nvim/lua/lsp/config/init.lua
@@ -1,14 +1,13 @@
+-- -*-mode:lua-*- vim:ft=lua
+
 local lsp_ok, lspconfig       = pcall(require, "lspconfig")
 local mason_cfg_ok, mason_cfg = pcall(require, "mason-lspconfig")
-local cmp_ok, cmp_nvim_lsp    = pcall(require, "cmp_nvim_lsp")
 local navic_ok, navic         = pcall(require, "nvim-navic")
 
 if not lsp_ok  then
   vim.notify('[lspconfig] Loading "lspconfig" failed.', vim.log.levels.WARN)
 elseif not mason_cfg_ok then
   vim.notify('[lspconfig] Loading "mason-lspconfig" failed.', vim.log.levels.WARN)
-elseif not cmp_ok then
-  vim.notify('[lspconfig] Loading "cmp_nvim_lsp" failed.', vim.log.levels.WARN)
 elseif not navic_ok then
   vim.notify('[lspconfig] Loading "nvim-navic" failed.', vim.log.levels.WARN)
 end
@@ -16,34 +15,16 @@ end
 require("lsp.config.handlers")
 
 local api, lsp = vim.api, vim.lsp
-local capabilities = cmp_nvim_lsp.default_capabilities(lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    "documentation",
-    "detail",
-    "additionalTextEdits",
-  }
-}
-capabilities.textDocument.colorProvider = {
-  dynamicRegistration = true
-}
-capabilities.textDocument.foldingRange  = {
-  dynamicRegistration = false,
-  lineFoldingOnly     = true,
-}
-
 local active_clients = lsp.get_active_clients()
 -- local inlay_hint     = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+-- local setup_inlay_hints = function()
+--   local hl = api.nvim_get_hl(0, { name = "Comment" })
+--   local foreground = string.format("#%06x", hl["fg"] or 0)
+--   if #foreground < 3 then foreground = "" end
 
--- -- local setup_inlay_hints = function()
--- --   local hl = api.nvim_get_hl(0, { name = "Comment" })
--- --   local foreground = string.format("#%06x", hl["fg"] or 0)
--- --   if #foreground < 3 then foreground = "" end
-
--- --   hl = api.nvim_get_hl(0, { name = "CursorLine" })
--- --   local background = string.format("#%06x", hl["bg"] or 0)
--- --   if #background < 3 then background = "" end
+--   hl = api.nvim_get_hl(0, { name = "CursorLine" })
+--   local background = string.format("#%06x", hl["bg"] or 0)
+--   if #background < 3 then background = "" end
 --   api.nvim_set_hl(0, 'LspInlayHint', { fg = foreground, bg = background })
 -- end
 
@@ -89,7 +70,7 @@ mason_cfg.setup({ ensure_installed = servers })
 
 local server_opts = {
   on_attach    = on_attach,
-  capabilities = capabilities,
+  capabilities = require("lsp.config.capabilities"),
 }
 mason_cfg.setup_handlers {
   function (server)
