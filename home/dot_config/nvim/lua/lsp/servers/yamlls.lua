80737d903d8bb92355c577ce7d0c0e592c8f34e6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-18 05:50:20 +0900

    chore(neovim): update `yamlls` config

--- home/dot_config/nvim/lua/lsp/servers/yamlls.lua
+++ home/dot_config/nvim/lua/lsp/servers/yamlls.lua
@@ -1,3 +1,4 @@
+-- -*-mode:lua-*- vim:ft=lua
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then return end
 
@@ -6,12 +7,21 @@ return {
   root_dir  = lspconfig.util.find_git_ancestor,
   filetypes = { "yaml", "yml", "yaml.docker-compose" },
   settings  = {
+    -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
+    redhat = { telemetry = { enabled = false } },
     yaml = {
       keyOrdering = false,
       schemaStore = {
         enable = true,
         url = "https://www.schemastore.org/api/json/catalog.json",
-      }
-    }
-  }
+      },
+      format = {
+        enabled = true,
+        singleQuote = false,
+      },
+      validate = true,
+      completion = true,
+      hover = true
+    },
+  },
 }
