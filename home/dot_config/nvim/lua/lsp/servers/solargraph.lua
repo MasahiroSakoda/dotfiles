fd1a9c15e87aa6799a819b16cabae046db5767ed
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-18 05:37:34 +0900

    chore(neovim): update `solargraph` config

--- home/dot_config/nvim/lua/lsp/servers/solargraph.lua
+++ home/dot_config/nvim/lua/lsp/servers/solargraph.lua
@@ -1,14 +1,19 @@
+-- -*-mode:lua-*- vim:ft=lua
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then return end
 
+local ft = require("user.filetypes")
 return {
   cmd = { "solargraph", "stdio" },
+  filetypes = { "ruby" },
+  root_dir  = lspconfig.util.root_pattern(ft.lsp.ruby),
+
   settings = {
     solargraph = {
+      hint = { enable = true },
       diagnostics = true,
+      formatting  = true,
+      completion  = true,
     },
   },
-  init_options = { formatting = true },
-  filetypes = { "ruby" },
-  root_dir  = lspconfig.util.root_pattern("Gemfile", ".git"),
 }
