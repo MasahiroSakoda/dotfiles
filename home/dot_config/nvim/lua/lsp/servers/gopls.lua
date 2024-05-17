36909f1466dd8f66609054e42c484fa20fd6bcd0
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-18 04:57:14 +0900

    chore(neovim): update `gopls` lspconfig
    typically confliction with `revive`

--- home/dot_config/nvim/lua/lsp/servers/gopls.lua
+++ home/dot_config/nvim/lua/lsp/servers/gopls.lua
@@ -7,12 +7,36 @@ return {
   root_dir  = lspconfig.util.root_pattern("go.mod", "go.work", ".git"),
   settings  = {
     gopls = {
+      gofumpt = true,
+      semanticTokens = true,
+      staticcheck = false, -- leave it to `revive`
+      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+
       -- Completion settings
       usePlaceholders = true,
       completeUnimported = true,
       experimentalPostfixCompletions = true,
       completionDocumentation = true,
       deepCompletion = true,
+
+      -- Inlay hints
+      ["ui.inlayhint.hints"] = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        constantValues = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+
+      -- codelens
+      codelenses = {
+        generate = true, -- show the `go generate` lens.
+        gc_details = false, --  // Show a code lens toggling the display of gc's choices.
+        test = true,
+        tidy = true,
+        vendor = true,
+        upgrade_dependency = true,
+      },
 
       -- the following diagnostics options are leave it to `revive`
       -- staticcheck = true,
@@ -21,6 +45,7 @@ return {
       --   unreachable  = true,
       --   shadow       = false,
       -- },
+      buildFlags = { os.getenv("GOFLAGS") }
     },
   },
   autostart = true,
