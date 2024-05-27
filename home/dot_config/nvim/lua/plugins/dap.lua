b423b1a0135b2fd821b15e9b9af71273bcc3092b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-05-28 00:29:53 +0900

    feat(neovim): update `nvim-dap-virtual-text` dependencies

--- home/dot_config/nvim/lua/plugins/dap.lua
+++ home/dot_config/nvim/lua/plugins/dap.lua
@@ -5,7 +5,6 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      { "theHamsta/nvim-dap-virtual-text", config = function() require("dap.config.virtual_text") end },
     },
     cmd    = { "DapToggleBreakpoint", "DapContinue", "DapTerminate", "DapStepOver", "DapStepInto", "DapStepOut" },
     config = function() require("dap.config") end,
@@ -13,6 +12,11 @@ return {
   {
     "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    config = function() require("dap.config.virtual_text") end,
   },
 
   {
