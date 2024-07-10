5c107e7cf2bdf3b0204b79541d343130084fa9f8
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-07-11 00:58:48 +0900

    fix(fish): check empty value to change local repo

--- home/dot_config/fish/functions/__ghq_cd_repository.fish
+++ home/dot_config/fish/functions/__ghq_cd_repository.fish
@@ -2,5 +2,7 @@
 
 function __ghq_cd_repository -d "Change local repository directory"
   ghq list --full-path | fzf | read -l repo_path
-  cd $repo_path
+  if [ $repo_path ]
+    cd $repo_path
+  end
 end
