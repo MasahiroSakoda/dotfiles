a1151e2a385d086c3e648d6af200ff351605e98d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-07-20 10:36:42 +0900

    feat(neovim): refine config for new version

--- home/dot_config/nvim/lua/lsp/config/trouble.lua
+++ home/dot_config/nvim/lua/lsp/config/trouble.lua
@@ -2,54 +2,20 @@ local ok, trouble = pcall(require, "trouble")
 if not ok then return end
 
 trouble.setup({
-  auto_open     = false,
-  auto_close    = false,
-  auto_preview  = false,
-  auto_fold     = false,
-  indent_lines  = true,
+  auto_close    = false, -- auto close when there are no items
+  auto_open     = false, -- auto open when there are items
+  auto_preview  = true,  -- automatically open preview when on an item
+  auto_refresh  = true,  -- auto refresh when open
+  auto_jump     = false, -- auto jump to the item when there's only one
+  focus         = true,  -- Focus the window when opened
+  restore       = true,  -- restores the last location in the list when opening
+  follow        = true,  -- Follow the current item
+  indent_guides = true,  -- show indent guides
+  max_items     = 200,   -- limit number of items that can be displayed per section
+  multiline     = true,  -- render multi-line messages
+  pinned        = false, -- When pinned, the opened trouble window will be bound to the current buffer
 
-  height = 10,
-  width  = 50,
-  icons  = true,
-  mode   = "workspace_diagnostics",
+  warn_no_results = false, -- show a warning when there are no results
+  open_no_results = true,  -- open the trouble window when there are no results
 
-  fold_open   = "",
-  fold_closed = "",
-  group       = true,
-  padding     = false,
-
-  signs = {
-    error       = "",
-    warning     = "",
-    hint        = "",
-    information = "",
-    other       = ""
-  },
-
-  action_keys   = {
-    close       = "q",
-    cancel      = { "<esc>", "<C-c>" },
-    previous    = "k",
-    next        = "j",
-    refresh     = "r",
-    jump        = {"<cr>", "<tab>"},
-    jump_close  = "o",
-    hover       = "zh",
-    preview     = "p",
-    toggle_preview = "P",
-
-    toggle_mode = "m",
-    open_split  = "<C-x>",
-    open_vsplit = "<C-v>",
-    open_tab    = "<C-t>",
-    open_folds  = { "zR", "zr" },
-    close_folds = { "zM", "zm" },
-    toggle_fold = { "zA", "za" },
-  },
 })
-
-vim.cmd[[ sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl= ]]
-vim.cmd[[ sign define DiagnosticSignWarn  text= texthl=DiagnosticSignWarn  linehl= numhl= ]]
-vim.cmd[[ sign define DiagnosticSignInfo  text= texthl=DiagnosticSignInfo  linehl= numhl= ]]
-vim.cmd[[ sign define DiagnosticSignHint  text= texthl=DiagnosticSignHint  linehl= numhl= ]]
-vim.cmd[[ sign define DiagnosticSignOther text= texthl=DiagnosticSignOther linehl= numhl= ]]
