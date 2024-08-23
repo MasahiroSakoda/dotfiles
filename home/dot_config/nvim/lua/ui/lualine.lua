c2cf4825ec6900f892c8d51c014b58793be30b48
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-08-23 21:08:03 +0900

    feat(neovim): update lualine sectioins

--- home/dot_config/nvim/lua/ui/lualine.lua
+++ home/dot_config/nvim/lua/ui/lualine.lua
@@ -23,38 +23,24 @@ lualine.setup {
     refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
   },
   sections = {
-    lualine_a = {
-      { "mode", icon = { "", color = { fg = "#0f0f0f" } } },
-    },
-    lualine_b = { "branch", "diff" },
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = {
       {
         "filename",
-        icon = { "", color = { fg = "#FFFFFF" } },
-        path = 1, --- @type 0|1|2|3|4
+        icon = "",
+        path = 1, -- 0: Filename, 1: Relative, 2: Absolute, 3: Absolute+ w/ home dir 4: Filename + Parent dir
         symbols = { modified = "", readonly = "", newfile  = "", unnamed  = "" }
       },
     },
     lualine_x = {
       {
-        -- TODO: Confirugre `overseer.nvim` detail options
-        "overseer",
-      },
-    },
-    lualine_y = { "encoding", "fileformat" },
-    lualine_z = { "progress", "location" },
-  },
-  winbar = {
-    lualine_a = {},
-    lualine_b = {
-      { "diagnostics", source = { "nvim-lsp", "nvim_diagnostic"} },
-    },
-    lualine_c = {
-      {
         function() return navic.get_location() end,
         cond = function() return navic.is_available() end,
       },
     },
+    lualine_y = { "overseer", "fileformat", "encoding" },
+    lualine_z = { "progress", "location" },
   },
   extensions = {
     "quickfix",
