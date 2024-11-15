048ba65fbc6b6f4654d786755dddbb278bdb40d0
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-11-15 21:10:20 +0900

    chore(neovim): remove unused dap plugin
    `microsoft/vscode-js-debug` & `mxdev/nvim-dap-vscode-js`

--- home/dot_config/nvim/lua/plugins/dap.lua
+++ home/dot_config/nvim/lua/plugins/dap.lua
@@ -1,5 +1,4 @@
 -- -*-mode:lua-*- vim:ft=lua
-local filetypes = require("user.filetypes")
 
 return {
   {
@@ -17,17 +16,5 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
     config = function() require("dap.config.virtual_text") end,
-  },
-
-  {
-    "microsoft/vscode-js-debug",
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-    ft    = filetypes.lang.js,
-    cond  = not vim.g.vscode,
-  },
-  {
-    "mxsdev/nvim-dap-vscode-js",
-    ft = filetypes.lang.js,
-    config = function() require("dap.adapters.javascript") end,
   },
 }
