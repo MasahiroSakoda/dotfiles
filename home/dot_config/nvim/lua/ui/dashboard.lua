a812ff2c2c65e746d5091a10d421548b91d6197f
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-11-25 02:40:34 +0900

    fix(neovim): remove option to pass ci

--- home/dot_config/nvim/lua/ui/dashboard.lua
+++ home/dot_config/nvim/lua/ui/dashboard.lua
@@ -1,19 +1,15 @@
 -- -*-mode:lua-*- vim:ft=lua
 
----@class snacks.dashboard.Config
 return {
   row = nil, -- dashboard position. nil for center
   col = nil, -- dashboard position. nil for center
   pane_gap = 4, -- empty columns between vertical panes
-  autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
 
   preset = {
-    ---@type fun(cmd:string, opts:table)|nil
     pick = nil,
     -- Used by the `keys` section to show keymaps.
     -- Set your curstom keymaps here.
     -- When using a function, the `items` argument are the default keymaps.
-    ---@type snacks.dashboard.Item[]
     keys = {
       { icon = " ", key = "n", desc = "New File",      action = ":ene | startinsert" },
       { icon = " ", key = "f", desc = "Frecency File", action = ":Telescope frecency" },
