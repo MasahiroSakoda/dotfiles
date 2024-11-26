0282c6abaedd782a343f78b36fc34ea07fd199aa
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-11-27 00:27:16 +0900

    feat(fish): update repository browsing feature using `ghq` & `fzf`
    add feture to preview README.md

--- home/dot_config/fish/conf.d/ghq.fish
+++
@@ -1,6 +0,0 @@
-# -*-mode:fish-*- vim:ft=fish
-
-bind \cg '__ghq_cd_repository'
-if bind -M insert >/dev/null 2>/dev/null
-  bind -M insert \cg '__ghq_cd_repository'
-end
--- home/dot_config/fish/functions/__ghq_cd_repository.fish
+++ home/dot_config/fish/functions/__ghq_cd_repository.fish
@@ -1,8 +1,15 @@
 # -*-mode:fish-*- vim:ft=fish
 
 function __ghq_cd_repository -d "Change local repository directory"
-  ghq list --full-path | fzf | read -l repo_path
-  if [ $repo_path ]
-    cd $repo_path
-  end
+  ghq list --full-path | \
+    fzf --preview "bat --color=always {}/README.md || echo 'No README.md'" \
+        --bind "ctrl-u:preview-up,ctrl-d:preview-down" | \
+    read -l repo_path
+
+  [ -n "$repo_path" ]; and cd "$repo_path"
+end
+
+bind \cg "__ghq_cd_repository"
+if bind -M insert >/dev/null 2>/dev/null
+  bind -M insert \cg "__ghq_cd_repository"
 end
