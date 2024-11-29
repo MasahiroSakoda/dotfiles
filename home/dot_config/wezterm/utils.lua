f0c1af0c99980883b4acf8d42304d3ea1c25c903
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-11-30 02:26:43 +0900

    chore(terminal): remove unused wezterm util function

--- home/dot_config/wezterm/utils.lua
+++ home/dot_config/wezterm/utils.lua
@@ -74,28 +74,6 @@ M.time = function()
   return fonts.pl_right_soft_divider .. " " .. fonts.fa_clock_o .. "  " .. time .. " "
 end
 
-M.battery_percentage = function()
-  local battery = ""
-  for _, b in ipairs(wezterm.battery_info()) do
-    local icon, charge = "", b.state_of_charge * 100
-    if charge <= 10 then
-      icon = fonts.md_battery_alert
-    elseif charge > 10 and charge <= 30 then
-      icon = fonts.md_battery_20
-    elseif charge > 30 and charge <= 50 then
-      icon = fonts.md_battery_40
-    elseif charge > 50 and charge <= 70 then
-      icon = fonts.md_battery_60
-    elseif charge > 70 and charge <= 90 then
-      icon = fonts.md_battery_80
-    else
-      icon = fonts.md_battery
-    end
-    battery = icon .. " " .. string.format("%.0f%%", charge)
-  end
-  return battery
-end
-
 M.keyboard_status = function(window)
   local icon = window:composition_status() and "あ" or "abc"
   return fonts.pl_right_soft_divider .. " " .. icon .. " "
