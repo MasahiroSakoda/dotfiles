a5dc4c2571b8a793d2cb45fec51bf49aee506712
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-12-17 19:09:59 +0900

    chore(neovim): update dap config for `js` / `ts` environment

--- home/dot_config/nvim/lua/dap/adapters/init.lua
+++ home/dot_config/nvim/lua/dap/adapters/init.lua
@@ -1,11 +1,10 @@
 -- -*-mode:lua-*- vim:ft=lua
 
 return {
-  bash     = {},                                 -- bash-debug-adapter
-  codelldb = require("dap.adapters.codelldb"),   -- codelldb: C, C++, Rust
-  cpptools = require("dap.adapters.cpptools"),   -- cpptools: C, C++, Rust
-  delve    = require("dap.adapters.delve"),      -- Go
-  debugpy  = require("dap.adapters.debugpy"),    -- Python
-  js       = require("dap.adapters.javascript"), -- JavaScript, TypeScript
-  chrome   = require("dap.adapters.chrome"),     -- Google Chrome
+  ["bash"]             = {},                                 -- bash-debug-adapter
+  ["codelldb"]         = require("dap.adapters.codelldb"),   -- codelldb: C, C++, Rust
+  ["cpptools"]         = require("dap.adapters.cpptools"),   -- cpptools: C, C++, Rust
+  ["delve"]            = require("dap.adapters.delve"),      -- Go
+  ["debugpy"]          = require("dap.adapters.debugpy"),    -- Python
+  ["js-debug-adapter"] = require("dap.adapters.typescript"), -- JavaScript, TypeScript
 }
--- home/dot_config/nvim/lua/dap/adapters/typescript.lua
+++ home/dot_config/nvim/lua/dap/adapters/typescript.lua
@@ -1,63 +1,107 @@
 local ok, dap = pcall(require, "dap")
 if not ok then return end
 
-dap.adapters.chrome = {
-  type    = "executable",
-  command = "node",
-  args = { vim.fn.stdpath("data") .. "/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js" },
-}
-local firefoxExecutable = "~/Applications/Firefox.app/Contents/MacOS/firefox"
+---@param pkg string Debug adapter name
+local adapter_path  = function(pkg)
+  return require("mason-registry").get_package(pkg):get_install_path()
+end
 
+-- Configure Debug Server
+local adapters  = { "pwa-node", "pwa-chrome" }
+local languages = { "javascript", "typescript", "javascriptreact", "typescriptreaact", "vue", "svelte" }
+
+for _, adapter in ipairs(adapters) do
+  if not dap.adapters[adapter] then
+    dap.adapters[adapter] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = { adapter_path("js-debug-adapter") .. "/js-debug/src/dapDebugServer.js", "${port}" },
+      },
+    }
+  end
+  require("dap.ext.vscode").type_to_filetypes[adapter] = languages
+end
+
+local firefoxExe = vim.fn.has("mac") and "/Applications/Firefox.app/Contents/MacOS/firefox" or "/usr/bin/firefox"
 dap.adapters.firefox = {
   type    = "executable",
   command = "node",
-  args = { vim.fn.stdpath("data") .. "/mason/packages/firefox-debug-adapter/out/src/firefoxDebug.js" },
+  args = { adapter_path("firefox-debug-adapter") .. "/dist/adapter.bundle.js" },
 }
 
-dap.configurations.typescript = {
-  {
-    name       = "Launch file",
-    type       = "node2",
-    request    = "launch",
-    program    = "${file}",
-    cwd        = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol   = "inspector",
-    console    = "integratedTerminal",
-  },
-  {
-    name       = "Attach to process",
-    type       = "node2",
-    sourceMaps = true,
-    request    = "attach",
-    processId  = require("dap.utils").pick_process,
-  },
-  -- TODO: Configure Chgome debug environment
-  {
-    name       = "Debug with Chrome",
-    type       = "chrome",
-    request    = "attach",
-    program    = "${file}",
-    port       = 9222,
-    webRoot    = "${workspaceFolder}",
-    sourceMapPathOverrides = {
-      -- Sourcemap override for nextjs
-      ["webpack://_N_E/./*"] = "${webRoot}/*",
-      ["webpack:///./*"]     = "${webRoot}/*",
-    },
-  },
-  -- TODO: Configure Firefox debug environment
-  {
-    name       = "Debug with Firefox",
-    type       = "firefox",
-    request    = "launch",
-    reAttach   = true,
-    sourceMaps = true,
-    url        = "http://localhost:6969",
-    webRoot    = "",
-    firefoxExecutable = firefoxExecutable,
-  }
-}
--- dap.configurations.javascript      = dap.configurations.typescript
--- dap.configurations.javascriptreact = dap.configurations.typescript
--- dap.configurations.typescriptreact = dap.configurations.typescript
+-- Configure debugger client config
+for _, lang in ipairs(languages) do
+  local isTS = lang == ("typescript" or "typescriptreaact")
+  if not dap.configurations[lang] then
+    dap.configurations[lang] = {
+      {
+        name        = "Launch Current File (pwa-node" .. (isTS and "with ts-node)" or ")"),
+        type        = "pwa-node",
+        request     = "launch",
+        program     = "${file}",
+        cwd         = "${workspaceFolder}",
+        protocol    = "inspector",
+        sourceMaps  = true,
+        console     = "integratedTerminal",
+        skipFiles   = { "<node_internals>/**", "node_modules/**" },
+        runtimeArgs = isTS and { "--loader", "=ts-node/esc" } or {},
+      },
+      {
+        name        = "Attach Program (pwa-node, select pid" .. (isTS and "with ts-node)" or ")"),
+        type        = "pwa-node",
+        request     = "attach",
+        sourceMaps  = true,
+        processId   = require("dap.utils").pick_process,
+        cwd         = "${workspaceFolder}",
+        runtimeArgs = isTS and { "--loader", "=ts-node/esc" } or {},
+      },
+      {
+        name     = "Launch Test Current File (pwa-node with jest)",
+        type     = "pwa-node",
+        request  = "launch",
+        trace    = true,
+        cwd      = "${workspaceFolder}",
+        rootPath = "${workspaceFolder}",
+        console  = "integratedTerminal",
+        runtimeExecutable = "node",
+        runtimeArgs       = { "./node_modules/jest/bin/jest.js", "--runInBand" },
+        internalConsoleOptions = "neverOpen",
+      },
+      {
+        name    = "Launch Test Current File (pwa-node with deno)",
+        type    = "pwa-node",
+        request = "launch",
+        cwd     = "${workspaceFolder}",
+        runtimeExecutable = "deno",
+        runtimeArgs       = { "test", "--inspect-brk", "--allow-all", "${file}" },
+        attachSimplePort  = 9229,
+      },
+      {
+        name    = "Attach Program (pwa-chrome)",
+        type    = "pwa-chrome",
+        request = "attach",
+        program = "${file}",
+        port    = 9222,
+        webRoot = "${workspaceFolder}",
+        sourceMapPathOverrides = {
+          -- Sourcemap override for nextjs
+          ["webpack://_N_E/./*"] = "${webRoot}/*",
+          ["webpack:///./*"]     = "${webRoot}/*",
+        },
+      },
+      {
+        name       = "Attach Program (firefox)",
+        type       = "firefox",
+        request    = "launch",
+        reAttach   = true,
+        sourceMaps = true,
+        url        = "http://localhost:6969",
+        webRoot    = "${workspaceFolder}",
+        firefoxExecutable = firefoxExe,
+      },
+    }
+  end
+end
