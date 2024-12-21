76e4f235039a0fcb2ca1fb76782a30e5baaf62e3
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-12-21 17:01:44 +0900

    chore(neovim): remove unused plugin config

--- home/dot_config/nvim/lua/lsp/servers/terraformls.lua
+++
@@ -1,9 +0,0 @@
-local ok, util = pcall(require, "lspconfig.util")
-if not ok then return end
-
-return {
-  default_config = {
-    cmd = { "terraform", "serve" },
-    filetypes = { "terraform", "tf", "terraform-vars", "hcl" },
-  },
-}
