63388e67289808f0d86790a6eebe9aec49d9d711
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-12-22 06:10:22 +0900

    chore(neovim): update `lualine.nvim` indicator config while LLM processing

--- home/dot_config/nvim/lua/ui/lualine.lua
+++ home/dot_config/nvim/lua/ui/lualine.lua
@@ -20,7 +20,7 @@ lualine.setup {
     ignore_focus = {},
     always_divide_middle = true,
     globalstatus = true,
-    refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
+    refresh = { statusline = 750, tabline = 1000, winbar = 1000 },
   },
   sections = {
     lualine_a = { "mode" },
