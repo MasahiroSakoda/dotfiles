59198042a5b0f661e115c0c0263ac145787295b9
MasahiroSakoda <thunderbird.m.s@gmail.com>
2024-12-23 23:44:37 +0900

    feat(neovim): add `nushell` config
    treesitter, lsp config

---
+++ home/dot_config/nvim/lua/lsp/servers/nushell.lua
@@ -0,0 +1,46 @@
+-- -*-mode:lua-*- vim:ft=lua
+
+-- INFO: settings to set nushell as the shell for the :! command
+-- --
+-- path to the Nushell executable
+vim.opt.sh = "nu"
+
+-- WARN: disable the usage of temp files for shell commands
+-- because Nu doesn't support `input redirection` which Neovim uses to send buffer content to a command:
+--      `{shell_command} < {temp_file_with_selected_buffer_content}`
+-- When set to `false` the stdin pipe will be used instead.
+-- NOTE: some info about `shelltemp`: https://github.com/neovim/neovim/issues/1008
+vim.opt.shelltemp = false
+
+  -- string to be used to put the output of shell commands in a temp file
+  -- 1. when 'shelltemp' is `true`
+  -- 2. in the `diff-mode` (`nvim -d file1 file2`) when `diffopt` is set
+  --    to use an external diff command: `set diffopt-=internal`
+  vim.opt.shellredir = "out+err> %s"
+
+-- flags for nu:
+-- * `--stdin`       redirect all input to -c
+-- * `--no-newline`  do not append `\n` to stdout
+-- * `--commands -c` execute a command
+vim.opt.shellcmdflag = "--stdin --no-newline -c"
+
+-- string to be used with `:make` command to:
+-- 1. save the stderr of `makeprg` in the temp file which Neovim reads using `errorformat` to populate the `quickfix` buffer
+-- 2. show the stdout, stderr and the return_code on the screen
+-- NOTE: `ansi strip` removes all ansi coloring from nushell errors
+vim.opt.shellpipe =
+  "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
+
+-- disable all escaping and quoting
+vim.opt.shellxescape = ""
+vim.opt.shellxquote  = ""
+vim.opt.shellquote   = ""
+
+return {
+  cmd      = { "nu", "--lsp" },
+  filetype = { "nu" },
+  root_dir = function(fname)
+    return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+  end,
+  single_file_support = true,
+}
