e3bca5ec6dca089eb40f6cc1e348eca00c92e1d6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-01-01 00:24:35 +0900

    feat(shell): add `zsh` abbreviation for `zellij`

---
+++ home/dot_config/zsh/functions/zellij.zsh
@@ -0,0 +1,29 @@
+# -*-mode:zsh-*- vim:ft=zsh
+
+function zellij-default() {
+  zellij --layout default
+}
+
+function zellij-coding() {
+  zellij --layout coding
+}
+
+function zellij-jsdev() {
+  zellij --layout jsdev
+}
+
+function zellij-monitor() {
+  zellij --layout monitor
+}
+
+function zellij-session() {
+  zellij attach $(zellij list-sessions | fzf --ansi | awk 'print $1')
+}
+
+function zellij-layout() {
+  local layout_dir="$XDG_DATA_HOME/zellij/layouts"
+  local layout="$(fd . ${layout_dir} --full-path -min-depth=1 --max-depth=1 --extension kdl | fzf --ansi)"
+  [ -n "$layout" ] && zellij --layout $layout
+  zle accept-line
+  zle reset-prompt
+}
