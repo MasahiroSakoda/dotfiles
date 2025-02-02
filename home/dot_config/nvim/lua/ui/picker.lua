5a88d1a4521eefaa4c566ad214f00d7e3e836b27
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-02-02 11:07:40 +0900

    feat(neovim): add `flash.nvim` integration with `Snacks.picker`

--- home/dot_config/nvim/lua/ui/picker.lua
+++ home/dot_config/nvim/lua/ui/picker.lua
@@ -9,4 +9,29 @@ return {
     cwd_bonus = true,
     frecency  = true,
   },
+
+  win = {
+    input = {
+      keys = {
+        ["<a-s>"] = { "flash", mode = { "n", "i" }},
+        ["s"]     = { "flash" },
+      },
+    },
+  },
+
+  actions = {
+    flash = function(picker)
+      require("flash").jump({
+        pattern = "^",
+        label = { after = { 0, 0 } },
+        search = {
+          mode = "search",
+          exclude = {
+            function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list" end,
+          },
+        },
+        action = function(match) picker.list:_move(picker.list:row2idx(match.pos[1]), true, true) end,
+      })
+    end,
+  },
 }
