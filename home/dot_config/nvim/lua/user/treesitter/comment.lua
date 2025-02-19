fc41440c8c7758cd3019012323fa2fd34b29a06c
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-02-20 04:46:46 +0900

    chore(neovim): add missing required options for `Comment.nvim`

--- home/dot_config/nvim/lua/user/treesitter/comment.lua
+++ home/dot_config/nvim/lua/user/treesitter/comment.lua
@@ -5,8 +5,15 @@ if not ok then return end
 vim.g.skip_ts_context_commentstring_module = true
 
 comment.setup({
-  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  padding   = true,
+  sticky    = true,
+  ignore    = "^$",
 
-  -- Ignore empty lines
-  ignore = "^$",
+  toggler   = { line = "gcc", block = "gbc" },
+  opleader  = { line = "gc", block = "gb" },
+  extra     = { above = "gcO", below = "gco", eol = "gcA" },
+  mappings  = { basic = true, extra = true },
+
+  pre_hook  = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  post_hook = function() end,
 })
