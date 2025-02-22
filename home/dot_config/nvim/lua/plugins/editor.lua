8d0e929d4ee431d715e6cef009c97da56d50793d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-02-22 17:29:21 +0900

    feat(neovim): move `snacks.nvim` config to specific file

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -28,26 +28,7 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy     = false,
-    opts = {
-      bigfile      = require("user.bigfile"),
-      quickfile    = require("user.quickfile"),
-      indent       = require("user.indent"),
-      toggle       = require("user.toggle"),
-      dashboard    = require("ui.dashboard"),
-      input        = require("ui.input"),
-      explorer     = require("ui.explorer"),
-      picker       = require("ui.picker"),
-      notifier     = require("ui.notifier"),
-      terminal     = require("ui.terminal"),
-      zen          = require("ui.zen"),
-      statuscolumn = { enabled = true },
-      words        = { enabled = false },
-      styles       = {
-        notification = {
-          wo = { wrap = true } -- Wrap notifications
-        },
-      },
-    }
+    config   = function() require("user.snacks") end,
   },
   -- Enhanced character motion
   {
