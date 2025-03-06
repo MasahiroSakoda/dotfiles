1283fd0900dc139ac2ef9cd4717d68871120ca7a
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-06 14:52:34 +0900

    chore(shell): update `fish` keybind to exec nvim plugin search with `fzf`

--- home/dot_config/fish/conf.d/ghq_key_bindings.fish
+++ home/dot_config/fish/conf.d/ghq_key_bindings.fish
@@ -1,6 +1,6 @@
 # -*-mode:fish-*- vim:ft=fish
 
-bind \cg "__ghq_browse_repository"
+bind ctrl-g "__ghq_browse_repository"
 if bind -M insert >/dev/null 2>/dev/null
   bind -M insert \cg "__ghq_browse_repository"
 end
