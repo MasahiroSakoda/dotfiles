3352ab5613560bb27d4223bc39e699317685ec0e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-14 01:35:16 +0900

    feat(neovim): update luasnip config
    load `friendly-snippets` & custom lua snippets

--- home/dot_config/nvim/lua/plugins/cmp.lua
+++ home/dot_config/nvim/lua/plugins/cmp.lua
@@ -29,7 +29,13 @@ return {
     event   = "InsertEnter",
     config  = function ()
       require("lsp.cmp.luasnip")
-      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({
+        override_priority = 1000,
+      })
+      require("luasnip.loaders.from_lua").load({
+        paths = {vim.fn.stdpath("config") .. "lua/snippets" },
+        override_priority = 2000,
+      })
     end,
   },
   {
