e703c704647021159c49433c97795585ea759907
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-15 15:19:47 +0900

    feat(neovim): add `sh` snippet

---
+++ home/dot_config/nvim/lua/snippets/sh.lua
@@ -0,0 +1,16 @@
+-- -*-mode:lua-*- vim:ft=lua
+local ls = require("luasnip")
+---@diagnostic disable-next-line: unused-local
+local s, t, i, c, r, f, sn =
+  ls.snippet,ls.text_node, ls.insert_node, ls.choice_node, ls.restore_node, ls.function_node, ls.snippet_node
+---@diagnostic disable-next-line: unused-local
+local fmt, fmta = require("luasnip.extras.fmt").fmt, require("luasnip.extras.fmt").fmta
+---@diagnostic disable-next-line: unused-local
+local conds, condse = require("luasnip.extras.conditions"), require("luasnip.extras.conditions.expand")
+---@diagnostic disable-next-line: unused-local
+local rep = require("luasnip.extras").rep
+
+local snippets = {
+  s("#!", fmt('#! /usr/bin/env bash\nset -euo pipefail', {})),
+}
+return snippets
