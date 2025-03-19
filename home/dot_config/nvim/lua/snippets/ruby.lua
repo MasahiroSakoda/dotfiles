1c122863152312a679730d16785887f3cf838b52
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-19 16:08:15 +0900

    chore(neovim): update ruby snippet format

--- home/dot_config/nvim/lua/snippets/ruby.lua
+++ home/dot_config/nvim/lua/snippets/ruby.lua
@@ -20,36 +20,54 @@ ls.filetype_extend("ruby",   { "rdoc", "rails" })
 
 local snippets = {
   -- Rails
-  s("controller", fmt('class {}Controller < ApplicationController\n\t{}\nend\n{}', { i(1, "name"), i(2, "code"), i(0) })),
-  s("helper",     fmt('module {}Helper\n\t{}\nend\n{}', { i(1, "name"), i(2, "code"), i(0) })),
+  s({ trig = "controller", name = "Rails Controller", dscr = "Rails controller component" },
+    fmt([[
+      class {}Controller < ApplicationController
+        {}
+      end
+      {}
+    ]], { i(1, "name"), i(2, "code"), i(0) })
+  ),
+  s({ trig = "helper", name = "Rails Helper", dscr = "Rails helper component" },
+    fmt([[
+      module {}Helper
+        {}
+      end
+      {}
+    ]], { i(1, "name"), i(2, "code"), i(0) })
+  ),
 
   -- Rspec
-  s("rspec", fmt('require "{}_helper"\n\nRspec.describe {}{} do\n\t{}\nend\n{}', {
-    c(1, {t("spec"), t("rails")}),
-    i(2),
-    c(3, {
-      fmt(', type: :{}', { i(1) }),
-      t(""),
-      t(", type: :model"),
-      t(", type: :controller"),
-      t(", type: :request"),
-      t(", type: :job"),
-      t(", type: :policy"),
-    }),
-    i(0),
-  })),
-  s("testf", fmt('require "test_helper"\n\nclass {}Test < ActiveSupport::TestCase\n\t{}\nend\n{}', {
-    i(1, "name"),
-    i(2, "code"),
-    i(0),
-  })),
-  s("it", fmt('{} do\n\t{}\nend\n{}', {
-    c(1, {
-      fmt('it "{}"', { i(1, "desc") }),
-      fmt('specify "{}"', i(1, "desc")),
-    }),
-    i(2, "code"),
-    i(0),
-  }))
+  s({ trig = "rspec", name = "Rspec template", dscr = "Rspec file template" },
+    fmt('require "{}_helper"\n\nRspec.describe {}{} do\n\t{}\nend\n{}', {
+      c(1, {t("spec"), t("rails")}),
+      i(2),
+      c(3, {
+        fmt(', type: :{}', { i(1) }),
+        t(""),
+        t(", type: :model"),
+        t(", type: :controller"),
+        t(", type: :request"),
+        t(", type: :job"),
+        t(", type: :policy"),
+      }),
+      i(0),
+    })
+  ),
+  s({ trig = "testf", name = "Rails test helper", dscr = "Rails test component" },
+    fmt('require "test_helper"\n\nclass {}Test < ActiveSupport::TestCase\n\t{}\nend\n{}', {
+      i(1, "name"), i(2, "code"), i(0),
+    })
+  ),
+  s({ trig = "it", name = "Rails test assert", dscr = "Rails test component" },
+    fmt('{} do\n\t{}\nend\n{}', {
+      c(1, {
+        fmt('it "{}"', { i(1, "desc") }),
+        fmt('specify "{}"', i(1, "desc")),
+      }),
+      i(2, "code"),
+      i(0),
+    })
+  )
 }
 return snippets
