56fa6ad0a311bf9c837606608800945a436352dd
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-20 04:12:36 +0900

    chore(neovim): specify `super-tab` preset keymap

--- home/dot_config/nvim/lua/lsp/cmp/blink.lua
+++ home/dot_config/nvim/lua/lsp/cmp/blink.lua
@@ -9,9 +9,22 @@ blink.setup({
   keymap = {
     preset = "super-tab",
     ["<C-d>"]   = { "show", "show_documentation", "hide_documentation", "fallback" },
+    ["<C-s>"]   = { "show_signature", "hide_signature", "fallback" },
     ["<C-c>"]   = { "cancel", "fallback" },
+    ["<C-e>"]    = { "hide", "fallback" },
+    ["<Tab>"]   = {
+      function(cmp) return cmp.snippet_active() and cmp.accept() or cmp.select_and_accept() end,
+      "snippet_forward",
+      "fallback",
+    },
+    ["<C-p>"]   = { "select_prev", "fallback" },
+    ["<C-n>"]   = { "select_next", "fallback" },
+    ["<C-b>"]   = { "scroll_documentation_up",   "fallback" },
+    ["<C-f>"]   = { "scroll_documentation_down", "fallback" },
+
     ["<Left>"]  = { "hide", "fallback" },
     ["<Right>"] = { "select_and_accept", "fallback" },
+
     -- ["<CR>"]    = { "select_and_accept" },
   },
 
