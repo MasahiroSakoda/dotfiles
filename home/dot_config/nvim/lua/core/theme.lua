638622653bae1ee664e235dec15ffeaa527ca9e1
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-21 04:11:10 +0900

    feat(neovim): update `Snacks.picker` highlights

--- home/dot_config/nvim/lua/core/theme.lua
+++ home/dot_config/nvim/lua/core/theme.lua
@@ -1,8 +1,9 @@
 -- -*-mode:lua-*- vim:ft=lua
-
 -- Color Schemes
 vim.cmd.colorscheme(vim.g.colorscheme)
 
 local palette = require("onedark.palette")[vim.g.themestyle]
-vim.api.nvim_set_hl(0, "SnacksPickerDir",        { fg = palette.cyan })
-vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { fg = palette.bg3 })
+vim.api.nvim_set_hl(0, "SnacksPickerDir",               { fg = palette.cyan })
+vim.api.nvim_set_hl(0, "SnacksPickerPathHidden",        { fg = palette.bg3 })
+vim.api.nvim_set_hl(0, "SnacksPickerSearch",            { fg = palette.red })
+vim.api.nvim_set_hl(0, "SnacksPickerPreviewCursorLine", { bg = palette.bg3 })
