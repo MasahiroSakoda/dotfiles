279d098a4519c47790b8eabc3d2f937a1f9cf674
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-22 11:53:45 +0900

    feat(neovim): add `typescriptreact` snippet typically for next.js

---
+++ home/dot_config/nvim/lua/snippets/typescriptreact.lua
@@ -0,0 +1,31 @@
+-- -*-mode:lua-*- vim:ft=lua
+local ls = require("luasnip")
+local s  = ls.snippet
+-- local t  = ls.text_node
+local i  = ls.insert_node
+-- local c  = ls.choice_node
+-- local r  = ls.restore_node
+-- local f  = ls.function_node
+-- local sn = ls.snippet_node
+-- local extras = require("luasnip.extras")
+-- local rep    = extras.rep
+-- local m      = extras.m
+-- local l      = extras.l
+local fmt    = require("luasnip.extras.fmt")
+-- local fmta   = require("luasnip.extras.fmta")
+-- local conds  = require("luasnip.extras.conditions")
+-- local condse = require("luasnip.extras.conditions.expand")
+
+local snippets = {
+
+  s({ trig = "nxp", name = "Next.js page", dscr = "Next.js page" },
+    fmt([[
+      export default function {}({}) {{
+        return (
+          {}
+        )
+      }}
+    ]], { i(1, "Page Name"), i(2, "Parameters"), i(0) })
+  ),
+}
+return snippets
