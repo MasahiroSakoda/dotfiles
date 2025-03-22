f67c5ec365af0d275a8d7e2597779cfeaf7fb04d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-22 14:40:20 +0900

    chore: suppress warning

--- home/dot_config/nvim/lua/snippets/fish.lua
+++ home/dot_config/nvim/lua/snippets/fish.lua
@@ -2,22 +2,22 @@
 ---@diagnostic disable: unused-local
 local ls = require("luasnip")
 local s  = ls.snippet
-local t  = ls.text_node
+-- local t  = ls.text_node
 local i  = ls.insert_node
-local c  = ls.choice_node
-local d  = ls.dynamic_node
-local r  = ls.restore_node
-local f  = ls.function_node
-local sn = ls.snippet_node
-local extras = require("luasnip.extras")
-local rep    = extras.rep
-local m      = extras.m
-local l      = extras.l
+-- local c  = ls.choice_node
+-- local d  = ls.dynamic_node
+-- local r  = ls.restore_node
+-- local f  = ls.function_node
+-- local sn = ls.snippet_node
+-- local extras = require("luasnip.extras")
+-- local rep    = extras.rep
+-- local m      = extras.m
+-- local l      = extras.l
 local fmt     = require("luasnip.extras.fmt").fmt
-local fmta    = require("luasnip.extras.fmt").fmta
-local postfix = require("luasnip.extras.postfix").postfix
-local conds   = require("luasnip.extras.conditions")
-local condse  = require("luasnip.extras.conditions.expand")
+-- local fmta    = require("luasnip.extras.fmt").fmta
+-- local postfix = require("luasnip.extras.postfix").postfix
+-- local conds   = require("luasnip.extras.conditions")
+-- local condse  = require("luasnip.extras.conditions.expand")
 
 local snippets = {
   s({ trig = "path", name = "path",    dscr = "fish_add_path template" },
