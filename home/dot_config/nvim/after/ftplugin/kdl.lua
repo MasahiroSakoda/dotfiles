3365e0f576e876893f245b9deeb72abc089bca90
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-26 02:07:30 +0900

    feat(neovim): unify indent config to `.editorconfig` instead of `after/ftplugin/*`

--- home/dot_config/nvim/after/ftplugin/kdl.lua
+++ home/dot_config/nvim/after/ftplugin/kdl.lua
@@ -1,4 +1,1 @@
 -- -*-mode:lua-*- vim:ft=lua
-vim.opt.shiftwidth  = 4
-vim.opt.tabstop     = 4
-vim.opt.softtabstop = 4
--- home/dot_config/nvim/after/ftplugin/nu.lua
+++ home/dot_config/nvim/after/ftplugin/nu.lua
@@ -1,6 +1,2 @@
 -- -*-mode:lua-*- vim:ft=lua
 vim.opt.shell = "nu"
-vim.opt.shiftwidth  = 4
-vim.opt.tabstop     = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = false
