b7cbcb7e3465fa92c057b74078ef0dd4a7382489
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-28 04:09:38 +0900

    feat(neovim): make gemini config up to date

--- home/dot_config/nvim/lua/ai/codecompanion/adapters/gemini.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/adapters/gemini.lua
@@ -5,7 +5,7 @@ return function()
       ---@see https://ai.google.dev/gemini-api/docs/models/gemini
       model = {
         default = "gemini-2.0-flash",
-        choices = { "gemini-2.0-flash", "gemini-1.5-flash-8b", "gemini-1.5-pro" },
+        choices = { "gemini-2.0-flash", "gemini-1.5-flash", "gemini-1.5-pro" },
       },
     },
     env = { api_key = vim.env.GEMINI_API_KEY },
