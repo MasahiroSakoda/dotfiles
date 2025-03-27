8179a93192a1c82af36e1d6a7a39268b62996ff6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-03-27 14:36:34 +0900

    feat(neovim): update highlight config

--- home/dot_config/nvim/lua/core/appearance.lua
+++ home/dot_config/nvim/lua/core/appearance.lua
@@ -1,35 +1,52 @@
-local opt, g, env = vim.opt, vim.g, vim.env
+-- -*-mode:lua-*- vim:ft=lua
+-- Color Schemes
+vim.cmd.colorscheme(vim.g.colorscheme)
 
-opt.title = false
-opt.shell = "fish"
+vim.opt.title = false
+vim.opt.shell = "fish"
 
 -- Line Numbers
-opt.number = true
-opt.relativenumber = false
-opt.numberwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.numberwidth = 2
 -- display line number for help
 vim.cmd[[autocmd! FileType help setlocal number]]
 
-opt.cursorline = true
+-- opt.cursorline   = true
+-- opt.cursorcolumn = true
+
+-- Cursor
+local palette = require("onedark.palette")[vim.g.themestyle]
+vim.opt.colorcolumn = { "120" }
+vim.opt.signcolumn  = "auto:1"
+vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=0, bg=palette.bg0 })
+vim.api.nvim_set_hl(0, "SignColumn",  { ctermbg=0, bg=palette.bg0 })
 
 -- Transparent window
-opt.winblend = 25
-opt.pumblend = 25
-g.floaterm_winblend = 35
+vim.opt.winblend = 25
+vim.opt.pumblend = 25
+vim.g.floaterm_winblend = 35
 
-opt.splitbelow = true
-opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
-env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
-opt.termguicolors = true
-opt.background    = "dark"
+vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
+vim.opt.termguicolors = true
+vim.opt.background    = "dark"
 
 -- Command
-opt.cmdheight  = 0
-opt.showcmd    = true
-opt.showmode   = true
+vim.opt.cmdheight  = 0
+vim.opt.showcmd    = true
+vim.opt.showmode   = true
 -- opt.ambiwidth  = 'double'
-opt.laststatus = 3
+vim.opt.laststatus = 3
 
-opt.list = true
-opt.listchars = { space = "･", eol = "↴", tab = "»-", trail = "-", nbsp = "%", extends = "⟩", precedes = "⟨" }
+vim.opt.list = true
+vim.opt.listchars = { space = "･", eol = "↴", tab = "»-", trail = "-", nbsp = "%", extends = "⟩", precedes = "⟨" }
+
+-- Highlights
+vim.api.nvim_set_hl(0, "SnacksPickerDir",               { fg = palette.cyan })
+vim.api.nvim_set_hl(0, "SnacksPickerFile",              { fg = palette.bg_yellow })
+vim.api.nvim_set_hl(0, "SnacksPickerPathHidden",        { fg = palette.bg3 })
+vim.api.nvim_set_hl(0, "SnacksPickerSearch",            { fg = palette.red })
+vim.api.nvim_set_hl(0, "SnacksPickerPreviewCursorLine", { bg = palette.bg3 })
