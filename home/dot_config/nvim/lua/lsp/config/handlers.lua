497da18aa467cdeda5933074e7995111f053777d
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-01 01:43:20 +0900

    refactor(neovim): replace deprecated func: `vim.lsp.handlers.hover` -> `vim.lsp.buf.hover`

--- home/dot_config/nvim/lua/lsp/config/handlers.lua
+++ home/dot_config/nvim/lua/lsp/config/handlers.lua
@@ -1,9 +1,6 @@
 -- -*-mode:lua-*- vim:ft=lua
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
-})
-
+vim.lsp.handlers["textDocument/hover"]         = vim.lsp.with(vim.lsp.buf.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "rounded",
   silent = true,
