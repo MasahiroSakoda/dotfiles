e496e277b215d751b7dc9e25ce7c63f8a5cd80b4
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-01 01:44:50 +0900

    refactor(neovim): replace `vim.lsp.handlers.signature_help` -> `vim.lsp.buf.signature_help`

--- home/dot_config/nvim/lua/lsp/config/handlers.lua
+++ home/dot_config/nvim/lua/lsp/config/handlers.lua
@@ -1,7 +1,7 @@
 -- -*-mode:lua-*- vim:ft=lua
 
 vim.lsp.handlers["textDocument/hover"]         = vim.lsp.with(vim.lsp.buf.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.buf.signature_help, {
   border = "rounded",
   silent = true,
   -- focusable = false,
