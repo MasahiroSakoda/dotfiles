c96046f7417d25a1557ef99e49f8e0b29360e18e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-06 01:04:35 +0900

    refactor(neovim): ``tidy up adapters & prompts

--- home/dot_config/nvim/lua/ai/codecompanion/init.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/init.lua
@@ -8,26 +8,8 @@ codecompanion.setup({
     language  = "English", ---@type "English"|"Japanese"
   },
 
-  prompt_library = {
-    ["Documentation"]         = require("ai.codecompanion.prompts.documentation"),
-    ["Refactor Code in chat"] = require("ai.codecompanion.prompts.refactor_chat"),
-    ["Refactor Code inline"]  = require("ai.codecompanion.prompts.refactor_inline"),
-    ["Review"]                = require("ai.codecompanion.prompts.review"),
-    ["PullRequest"]           = require("ai.codecompanion.prompts.pull_request"),
-    ["Correct Grammar"]       = require("ai.codecompanion.prompts.grammar"),
-    ["Translate"]             = require("ai.codecompanion.prompts.translate"),
-    ["Summarize"]             = require("ai.codecompanion.prompts.summarize"),
-  },
-
-  adapters = {
-    groq      = require("ai.codecompanion.adapters.groq"),
-    copilot   = require("ai.codecompanion.adapters.copilot"),
-    openai    = require("ai.codecompanion.adapters.openai"),
-    anthropic = require("ai.codecompanion.adapters.anthropic"),
-    google    = require("ai.codecompanion.adapters.google"),
-    qwen      = require("ai.codecompanion.adapters.qwen"),
-    dolhin    = require("ai.codecompanion.adapters.dolphin"),
-  },
+  prompt_library = require("ai.codecompanion.prompts"),
+  adapters       = require("ai.codecompanion.adapters"),
 
   strategies = {
     chat   = { adapter = "qwen" },
