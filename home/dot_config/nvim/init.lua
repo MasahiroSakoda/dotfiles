5092918c6df41ab835be312655a60a64565c3832
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-11 04:35:42 +0900

    chore(neovim): update `localleader` for `octo.nvim` to prevent conflict w/ toggle keymaps

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -15,7 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader      = [[ ]]
-vim.g.maplocalleader = [[,]]
+vim.g.maplocalleader = [[\]]
 vim.g.colorscheme = "onedark" --- @type "nightfox"|"dayfox"|"dawnfox"|"duskfox"|"nordfox"|"carbonfox"|"onedark"
 vim.g.themestyle  = "darker"
 
