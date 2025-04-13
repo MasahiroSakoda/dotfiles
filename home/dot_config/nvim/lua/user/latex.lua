3760863b93b9aca73a6d64880dae0a71492cef57
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-13 13:13:23 +0900

    feat(neovim): configure `vimtex`

---
+++ home/dot_config/nvim/lua/user/latex.lua
@@ -0,0 +1,30 @@
+-- -*-mode:lua-*- vim:ft=lua
+
+vim.g.tex_flavor             = "latex"
+vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_quickfix_mode   = 0 -- Disable auto popup of quickfix window
+-- vim.g.vimtex_fold_enabled    = true
+vim.g.vimtex_syntax_conceal = {
+  accents         = true,
+  ligatures       = true,
+  cites           = true,
+  fancy           = true,
+  spacing         = false, -- default: true
+  greek           = true,
+  math_bounds     = true,
+  math_delimiters = true,
+  math_fracs      = true,
+  math_super_sub  = true,
+  math_symbols    = true,
+}
+
+local uname = vim.uv.os_uname().sysname
+if uname == "Darwin" then
+  vim.g.vimtex_view_method         = "skim"
+  vim.g.vimtex_view_general_viewer = "skim"
+  vim.g.vimtex_view_skim_sync      = true
+  vim.g.vimtex_view_skim_activate  = true
+elseif uname == "Linux" then
+  vim.g.vimtex_view_method         = "zathura"
+  vim.g.vimtex_view_general_viewer = "zathura"
+end
