b123fe5fc15cba7b21c5570903e5f25b286e6d1b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-16 03:56:52 +0900

    feat(neovim): add custom command to toggle `onedark.nvim` theme style
    to display description for `which-key.nvim`

--- home/dot_config/nvim/lua/ui/onedark.lua
+++ home/dot_config/nvim/lua/ui/onedark.lua
@@ -6,8 +6,7 @@ if not onedark_ok then return end
 onedark.setup({
   style = vim.g.themestyle, ---@type "dark"|"darker"|"cool"|"deep"|"warm"|"warmer"
   transparent = true,
-
-  toggle_style_key = ",C",
+  toggle_style_key = "<NOP>",
 
   code_style = {
     comments  = "italic",
@@ -16,3 +15,8 @@ onedark.setup({
     types     = "italic,bold",
   },
 })
+
+vim.api.nvim_create_user_command("ToggleOneDarkStyle", function(_)
+  onedark.toggle()
+  vim.notify(string.format("%s", vim.g.onedark_config.style), vim.log.levels.INFO)
+end, { desc = "Toggle OneDark theme style", nargs = "*", bang = true })
