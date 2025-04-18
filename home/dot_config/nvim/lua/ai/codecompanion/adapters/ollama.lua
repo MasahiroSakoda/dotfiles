9633485ac6393f52519076d2d07b7dad3cf9ca25
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-19 04:58:43 +0900

    feat(neovim): update `codecompanion.nvim` atapter for generic usage
    `dolphin3:latest` -> `cogito:8b`

--- home/dot_config/nvim/lua/ai/codecompanion/adapters/ollama.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/adapters/ollama.lua
@@ -2,7 +2,7 @@
 
 return function()
   return require("codecompanion.adapters").extend("ollama", {
-    name   = "Dolphin 3.0 based Llama 3.1 8B",
-    schema = { model = { default = "dolphin3:latest" } },
+    name   = "Cogito v1 preview",
+    schema = { model = { default = "cogito:8b" } },
   })
 end
