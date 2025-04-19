bc9ae2155d4a31953bc38890338b88f32bb437ba
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-04-20 05:04:50 +0900

    refactor(neovim): refine prompt for translate

--- home/dot_config/nvim/lua/ai/codecompanion/prompts/translate.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/prompts/translate.lua
@@ -17,22 +17,28 @@ return {
   prompts = {
     {
       role = "system",
-      content = [[
-I want you to act as an ]] .. target_lang .. [[ translator, spelling corrector and improver.
+      content = function(_)
+        return string.format([[
+I want you to act as an %s translator, spelling corrector and improver.
 I will speak to you in any language and you will detect the language,
-translate it and answer in the corrected and improved version of my text, in ]] .. target_lang .. [[.
+translate it and answer in the corrected and improved version of my text, in %s.
 I want you to replace my simplified A0-level words and sentences with more beautiful and elegant,
-upper level ]] .. target_lang .. [[ words and sentences.
+upper level %s words and sentences.
 Keep the meaning same, but make them more literary.
 I want you to only reply the correction, the improvements and nothing else, do not write explanations."
-      ]],
+        ]], target_lang, target_lang, target_lang)
+      end,
     },
     {
       role = "user",
       content = function(ctx)
         local text = require("codecompanion.helpers.actions").get_code(ctx.start_line, ctx.end_line)
-        return "Please translate this text to " .. target_lang .. ".\n" ..
-          "Return the translation only and no markdown codeblocks:\n\n" .. text
+        return string.format([[
+Please translate this text to %s.
+Return the translation only and no markdown codeblocks:
+
+%s
+        ]], target_lang, text)
       end,
     },
   },
