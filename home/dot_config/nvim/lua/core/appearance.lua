3859c7383ada629b6f9237b63139457aea8806ba
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-05-10 13:10:42 +0900

    feat(neovim): update `Snacks.picker` highlight for matched string

--- home/dot_config/nvim/lua/core/appearance.lua
+++ home/dot_config/nvim/lua/core/appearance.lua
@@ -45,5 +45,5 @@ vim.opt.listchars = { space = "･", eol = "↴", tab = "»-", trail = "-", nbsp
 vim.api.nvim_set_hl(0, "SnacksPickerDir",               { fg = palette.cyan })
 vim.api.nvim_set_hl(0, "SnacksPickerFile",              { fg = palette.bg_yellow })
 vim.api.nvim_set_hl(0, "SnacksPickerPathHidden",        { fg = palette.bg3 })
-vim.api.nvim_set_hl(0, "SnacksPickerSearch",            { fg = palette.red })
+vim.api.nvim_set_hl(0, "SnacksPickerMatch",             { fg = palette.red })
 vim.api.nvim_set_hl(0, "SnacksPickerPreviewCursorLine", { bg = palette.bg3 })
