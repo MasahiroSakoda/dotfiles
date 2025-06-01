b297bb4b879cc801116933fc42b8377b932ba788
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-06-01 16:46:58 +0900

    chore(neovim): update `OverseerRestartLast` command

--- home/dot_config/nvim/lua/user/overseer.lua
+++ home/dot_config/nvim/lua/user/overseer.lua
@@ -27,9 +27,11 @@ overseer.setup({
 -- Latest task command
 vim.api.nvim_create_user_command("OverseerRestartLast", function()
   local tasks = overseer.list_tasks({ recent_first = true })
-  return vim.tbl_isempty(tasks) and
-    vim.notify("No tasks found", vim.log.levels.WARN) or
+  if vim.tbl_isempty(tasks) then
+    vim.notify("No tasks found", vim.log.levels.WARN)
+  else
     overseer.run_action(tasks[1], "restart")
+  end
 end, {})
 
 -- Async make command
