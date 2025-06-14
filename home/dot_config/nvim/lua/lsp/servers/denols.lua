84d30cbf968b897fc90adc07dd5037586d0cfb48
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-06-15 03:31:57 +0900

    feat(neovim): update `denols` config

--- home/dot_config/nvim/lua/lsp/servers/denols.lua
+++ home/dot_config/nvim/lua/lsp/servers/denols.lua
@@ -1,51 +1,50 @@
 -- -*-mode:lua-*- vim:ft=lua
 
-local lsp, api = vim.lsp, vim.api
-local virtual_text_document_handler = function(uri, result)
-  if not result then
-    return
+local function virtual_text_document_handler(uri, res, client)
+  if not res then
+    return nil
   end
-  for client_id, res in ipairs(result) do
-    local lines = vim.split(res.result, "\n")
-    local bufnr = vim.uri_to_bufnr(uri)
 
-    local current_buf = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    if #current_buf ~= 0 then
-      return nil
-    end
+  local lines = vim.split(res.result, "\n")
+  local bufnr = vim.uri_to_bufnr(uri)
 
-    api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-    vim.bo.readonly   = true
-    vim.bo.modified   = true
-    vim.bo.modifiable = true
-    lsp.buf_attach_client(bufnr, client_id)
+  local current_buf = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  if #current_buf ~= 0 then
+    return nil
   end
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.api.nvim_set_options_value("readonly",   true,  { buf = bufnr })
+  vim.api.nvim_set_options_value("modified",   false, { buf = bufnr })
+  vim.api.nvim_set_options_value("modifiable", false, { buf = bufnr })
+  vim.lsp.buf_attach_client(bufnr, client.id)
 end
 
-local virtual_text_document = function(uri)
+local function virtual_text_document(uri, client)
   local params = {
     textDocument = {
       uri = uri,
     },
   }
-  local result = lsp.buf_request_sync(0, "deno/virtualTextDocument", params)
-  virtual_text_document_handler(uri, result)
+  local result = vim.lsp.buf_request_sync(0, "deno/virtualTextDocument", params)
+  virtual_text_document_handler(uri, result, client)
 end
 
-local denols_handler = function(err, result, ctx)
+local function denols_handler(err, result, ctx, config)
   if not (result or vim.tbl_isempty(result)) then
     return nil
   end
 
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
   for _, res in ipairs(result) do
     local uri = res.uri or res.targetUri
     if uri:match "^deno:" then
-      virtual_text_document(uri)
-      res['uri'] = uri
-      res['targetUri'] = uri
+      virtual_text_document(uri, client)
+      res["uri"]       = uri
+      res["targetUri"] = uri
     end
   end
-  lsp.handlers[ctx.method](err, result, ctx)
+  vim.lsp.handlers[ctx.method](err, result, ctx, config)
 end
 
 local ft = require("user.filetypes")
@@ -54,7 +53,8 @@ local ft = require("user.filetypes")
 return {
   cmd          = { "deno", "lsp" },
   root_markers = ft.lsp.deno,
-  autostart = true,
+  filetypes    = ft.lang.js,
+  autostart    = true,
   init_options = {
     enable    = true,
     lint      = true,
@@ -69,37 +69,40 @@ return {
       },
     },
   },
-  filetypes = ft.lang.js,
   handlers = {
-    ["textDocument/definition"] = denols_handler,
-    ["textDocument/references"] = denols_handler,
+    ["textDocument/definition"]     = denols_handler,
+    ["textDocument/typeDefinition"] = denols_handler,
+    ["textDocument/references"]     = denols_handler,
   },
 
   settings = {
     deno = {
       suggest = { completeFunctionCalls = true },
       inlayHints = {
-        enumMemberValues = {
-          enabled = true,
-        },
-        functionLikeReturnTypes = {
-          enabled = true,
-        },
-        parameterNames = {
-          enabled = true,
-          suppressWhenArgumentMatchesName = true,
-        },
-        parameterTypes = {
-          enabled = true,
-        },
-        propertyDeclarationTypes = {
-          enabled = true,
-        },
-        variableTypes = {
-          enabled = true,
-          suppressWhenTypeMatchesName = true,
-        },
+        enumMemberValues          = { enabled = true },
+        functionLikeReturnTypes   = { enabled = true },
+        parameterNames            = { enabled = true, suppressWhenArgumentMatchesName = true },
+        parameterTypes            = { enabled = true },
+        propertyDeclarationTypes  = { enabled = true },
+        variableTypes             = { enabled = true, suppressWhenTypeMatchesName = true },
       },
     },
   },
+
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(0, "LspDenolsCache", function()
+      client:exec_cmd({
+        title = "",
+        command = "deno.cache",
+        arguments = {{}, vim.uri_from_bufnr(bufnr)},
+      }, { bufnr = bufnr },
+        function(err, _, ctx)
+          if err then
+            local uri = ctx.params.arguments[2]
+            vim.api.nvim_echo({ { "cache command failed for " .. vim.uri_to_fname(uri) } }, true, { err = true })
+          end
+        end
+      )
+    end, {desc = "Cache a module and all of its dependencies."})
+  end,
 }
