6bf9c36e9e45aec6138ac48716d37bc4b8738384
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-06-16 03:33:47 +0900

    feat(neovim): update diagnostic config
    use `virtual_lines` instead of `virtual_text`

--- home/dot_config/nvim/lua/lsp/config/diagnostics.lua
+++ home/dot_config/nvim/lua/lsp/config/diagnostics.lua
@@ -1,13 +1,20 @@
 -- -*-mode:lua-*- vim:ft=lua
 
----@see https://eiji.page/blog/neovim-diagnostic-config/
+---@type vim.diagnostic.Opts
 vim.diagnostic.config({
-  underline     = true,
   severity_sort = true,
-  severity      = { min = vim.diagnostic.severity.HINT },
-  virtual_text  = {
-    format = function(diagnostic)
-      return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-    end
+  severity      = { min = vim.diagnostic.severity.HINT, max = vim.diagnostic.severity.ERROR },
+  underline     = { severity = vim.diagnostic.severity.ERROR },
+  jump          = { float = true },
+  virtual_lines = {
+    current_line = false,
+    format = function(d)
+      return string.format("%s (%s: %s)", d.message, d.source, d.code)
+    end,
   },
+  -- virtual_text  = {
+  --   format = function(d)
+  --     return string.format("%s (%s: %s)", d.message, d.source, d.code)
+  --   end,
+  -- },
 })
