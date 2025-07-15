38a533b47843e9d965ab6641a1e8c630cb536b25
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-07-15 17:28:55 +0900

    feat(neovim): add line number config for `gitcommit` filetype

--- home/dot_config/nvim/after/ftplugin/gitcommit.lua
+++ home/dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -1,5 +1,7 @@
 -- -*-mode:lua-*- vim:ft=lua
 
+vim.opt_local.number         = false
+vim.opt_local.relativenumber = false
 vim.opt_local.linebreak      = true
 vim.opt_lcoal.spell          = true
 vim.opt_lcoal.spelllang      = "en"
