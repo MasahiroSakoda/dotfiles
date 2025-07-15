2871c7af2ae033607dfc74a274085fcf4ccc5572
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-07-15 19:38:50 +0900

    feat(neovim): update `help` filetype config

--- home/dot_config/nvim/after/ftplugin/help.lua
+++ home/dot_config/nvim/after/ftplugin/help.lua
@@ -1,7 +1,13 @@
-local opt = vim.opt
-opt.list   = false
-opt.wrap   = false
-opt.spell  = false
-opt.number = false
-opt.relativenumber = false
+-- -*-mode:lua-*- vim:ft=lua
+
+vim.diagnostic.enable(false, { bufnr = 0 })
+
+vim.opt_local.list   = false
+vim.opt_local.wrap   = true
+vim.opt_local.spell  = false
+
+vim.opt_local.number         = false
+vim.opt_local.relativenumber = false
+
 vim.b.cursorword = 0
+vim.cmd.wincmd("L")
