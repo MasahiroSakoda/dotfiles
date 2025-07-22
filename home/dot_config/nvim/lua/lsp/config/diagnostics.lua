daa0fb8d96b38b9e83948f7c850bad0fec27d3ab
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-07-23 00:06:27 +0900

    feat(neovim): reactivate neovim lsp diagnostics virtual line with addtional option

--- home/dot_config/nvim/lua/lsp/config/diagnostics.lua
+++ home/dot_config/nvim/lua/lsp/config/diagnostics.lua
@@ -6,15 +6,11 @@ vim.diagnostic.config({
   severity      = { min = vim.diagnostic.severity.HINT, max = vim.diagnostic.severity.ERROR },
   underline     = { severity = vim.diagnostic.severity.ERROR },
   jump          = { float = true },
-  virtual_lines = {
-    current_line = false,
+  virtual_text  = {
+    prefix = "●",
+    spacing = 2,
     format = function(d)
       return string.format("%s (%s: %s)", d.message, d.source, d.code)
     end,
   },
-  -- virtual_text  = {
-  --   format = function(d)
-  --     return string.format("%s (%s: %s)", d.message, d.source, d.code)
-  --   end,
-  -- },
 })
