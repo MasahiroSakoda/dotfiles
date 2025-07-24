7e7cf2c9b5c1f5d0bb36ebb6b6a2bc6e9b595782
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-07-24 21:33:22 +0900

    feat(neovim): update `biome` config
    add missing filetype

--- home/dot_config/nvim/lua/lsp/servers/biome.lua
+++ home/dot_config/nvim/lua/lsp/servers/biome.lua
@@ -1,9 +1,14 @@
 -- -*-mode:lua-*- vim:ft=lua
 local ft = require("user.filetypes")
+local cmd = vim.fn.executable("pnpm") and "pnpm" or "npm"
 
 ---@type vim.lsp.Config
 return {
-  cmd          = vim.fn.executable("pnpm") and { "pnpm", "biome", "lsp-proxy" } or { "npx", "biome", "lsp-proxy" },
-  filetypes    = ft.lang.js,
+  cmd       = { cmd, "biome", "lsp-proxy" },
+  filetypes = {
+    "javascript", "javascriptreact", "typescript", "typescriptreact",
+    "json", "jsonc",
+    "html", "css", "vue", "svelte", "astro",
+  },
   root_markers = ft.lsp.biome,
 }
