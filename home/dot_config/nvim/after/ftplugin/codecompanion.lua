171b8e3216ceffda6caff71504163173d4204bd1
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-08-06 04:07:09 +0900

    feat(neovim): update filetype config for `codecompanion`

---
+++ home/dot_config/nvim/after/ftplugin/codecompanion.lua
@@ -0,0 +1,8 @@
+-- -*-mode:lua-*- vim:ft=lua
+
+vim.schedule(function()
+  vim.opt_local.number       = false
+  vim.opt_local.number       = false
+  vim.opt_local.cursorcolumn = false
+  vim.opt_local.colorcolumn  = {}
+end)
