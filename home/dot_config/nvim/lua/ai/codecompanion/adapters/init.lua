348ed58b2fb147112f555fd2b363ff516c83e001
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-15 03:12:14 +0900

    feat(ai): update `blink.cmp` deprecated adapter config
    deprecate in 18.x

--- home/dot_config/nvim/lua/ai/codecompanion/adapters/init.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/adapters/init.lua
@@ -1,11 +1,13 @@
 -- -*-mode:lua-*- vim:ft=lua
 
 return {
-  groq      = require("ai.codecompanion.adapters.groq"),
-  copilot   = require("ai.codecompanion.adapters.copilot"),
-  openai    = require("ai.codecompanion.adapters.openai"),
-  anthropic = require("ai.codecompanion.adapters.anthropic"),
-  google    = require("ai.codecompanion.adapters.google"),
-  qwen      = require("ai.codecompanion.adapters.qwen"),
-  ollama    = require("ai.codecompanion.adapters.ollama"),
+  http = {
+    groq      = require("ai.codecompanion.adapters.groq"),
+    copilot   = require("ai.codecompanion.adapters.copilot"),
+    openai    = require("ai.codecompanion.adapters.openai"),
+    anthropic = require("ai.codecompanion.adapters.anthropic"),
+    google    = require("ai.codecompanion.adapters.google"),
+    qwen      = require("ai.codecompanion.adapters.qwen"),
+    ollama    = require("ai.codecompanion.adapters.ollama"),
+  },
 }
