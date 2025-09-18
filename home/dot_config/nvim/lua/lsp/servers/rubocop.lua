3c423ddca46fbb623bfbf9f99ab0608e0aac45f2
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-18 11:34:17 +0900

    fix: typo

--- home/dot_config/nvim/lua/lsp/servers/rubocop.lua
+++ home/dot_config/nvim/lua/lsp/servers/rubocop.lua
@@ -2,7 +2,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd          = { "rubocop", "-lsp" },
+  cmd          = { "rubocop", "--lsp" },
   filetypes    = { "ruby" },
   root_markers = require("user.filetypes").lsp.ruby,
   single_file_support = true,
