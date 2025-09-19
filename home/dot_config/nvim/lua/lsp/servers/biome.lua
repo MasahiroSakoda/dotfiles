7f77db2a03f1dc1a3753a1a6398f03513ce9a232
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-20 04:23:03 +0900

    feat(neovim): update `biome` command via lsp

--- home/dot_config/nvim/lua/lsp/servers/biome.lua
+++ home/dot_config/nvim/lua/lsp/servers/biome.lua
@@ -1,10 +1,17 @@
 -- -*-mode:lua-*- vim:ft=lua
-local ft = require("user.filetypes")
-local cmd = vim.fn.executable("pnpm") and "pnpm" or "npm"
+local lspconfig, ft = require("lspconfig"), require("user.filetypes")
 
 ---@type vim.lsp.Config
 return {
-  cmd       = { cmd, "biome", "lsp-proxy" },
+  cmd = { "biome", "lsp-proxy" },
+  on_new_config = function(new_config)
+    local pnpm = lspconfig.util.root_pattern("pnpm-lock.yml", "pnpm-lock.yaml")(vim.api.nvim_buf_get_name(0))
+    local cmd = { "npx", "biome", "lsp-proxy" }
+    if pnpm then
+      cmd = { "pnpm", "biome", "lsp-proxy" }
+    end
+    new_config.cmd = cmd
+  end,
   filetypes = {
     "javascript", "javascriptreact", "typescript", "typescriptreact",
     "json", "jsonc",
