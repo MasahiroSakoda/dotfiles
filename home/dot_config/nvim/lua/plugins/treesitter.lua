0429bb7e7857d7d274f4bd69a5187c45ce4f30cd
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-24 16:47:31 +0900

    feat(neovim): change `textobjects` repo to `main` branch with `nvim-treesiter` branch migration

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
         config = function() require"user.treesitter.textobjects" end,
       },
       {
