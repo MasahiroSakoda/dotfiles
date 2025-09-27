7b603c202d828379dd2f3b3f2b8ca54dc1c99820
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-28 07:49:13 +0900

    chore(neovim): remove unused dependencies config

--- home/dot_config/nvim/lua/ui/codicons.lua
+++
@@ -1,9 +0,0 @@
-local ok, codicons = pcall(require, "codicons")
-if not ok then return end
-
-codicons.setup({})
-
-require("codicons.extensions.completion_item_kind")
-require("codicons.extensions.vs_code_product_icons")
-
-vim.g.vim_package_info_virutaltext_prefix = "  " .. codicons.get("versions", "icon") .. " "
