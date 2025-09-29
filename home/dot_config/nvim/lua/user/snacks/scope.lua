58eb36d1e30aba623f912158b32b81abdeae5b17
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-09-30 00:02:59 +0900

    feat(neovim): add `snacks.scope` keymaps
    - jump to scope top
    - jump to scope bottom

--- home/dot_config/nvim/lua/user/snacks/scope.lua
+++ home/dot_config/nvim/lua/user/snacks/scope.lua
@@ -1,6 +1,12 @@
 -- -*-mode:lua-*- vim:ft=lua
 local scope = require("snacks.scope")
 
+require("which-key").add({
+  mode = { "n", "x", "o" },
+  { "[i", function() scope.jump({ min_size = 1, bottom = false }) end, icon = " ", desc = "Scope Top" },
+  { "]i", function() scope.jump({ min_size = 1, bottom = true })  end, icon = " ", desc = "Scope Bottom" },
+})
+
 ---@type snacks.scope.Config
 return {
   enabled  = true,
