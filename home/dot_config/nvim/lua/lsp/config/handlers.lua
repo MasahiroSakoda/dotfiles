dc5266052a07ff77b0dfbf9c4ce97d51ebfe8377
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-10-10 10:02:17 +0900

    chore(neovim): remove deprecated: `vim.lsp.with`

--- home/dot_config/nvim/lua/lsp/config/handlers.lua
+++ home/dot_config/nvim/lua/lsp/config/handlers.lua
@@ -1,9 +1,1 @@
 -- -*-mode:lua-*- vim:ft=lua
-
-vim.lsp.handlers["textDocument/hover"]         = vim.lsp.with(vim.lsp.buf.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.buf.signature_help, {
-  border = "rounded",
-  silent = true,
-  -- focusable = false,
-  close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "InsertCharPre" },
-})
