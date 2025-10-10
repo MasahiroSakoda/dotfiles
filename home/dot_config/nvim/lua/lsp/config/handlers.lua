bcbaa8b608bf0f296663eacd1e96b74aad3da15e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-10-11 04:05:07 +0900

    feat(neovim): add custom lsp handler for `textDocument/hover`
    - supress "No Information Available" notifications

--- home/dot_config/nvim/lua/lsp/config/handlers.lua
+++ home/dot_config/nvim/lua/lsp/config/handlers.lua
@@ -1,1 +1,17 @@
 -- -*-mode:lua-*- vim:ft=lua
+
+-- Supress "No Information Available" notifications
+vim.lsp.handlers[vim.lsp.protocol.Methods.textDocument_hover] = function(_, result, _, config)
+  config = config or {}
+  if not (result and result.contents) then
+    return
+  end
+
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.split(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+
+  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end
