0b55733d7b5d6e48e0e3fcf3eec474a21271f5a8
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-10-16 06:09:13 +0900

    feat(neovim): update `quicker.nvim` config

--- home/dot_config/nvim/lua/user/quickfix/quicker.lua
+++ home/dot_config/nvim/lua/user/quickfix/quicker.lua
@@ -3,4 +3,17 @@ local ok, quicker = pcall(require, "quicker")
 if not ok then return end
 
 quicker.setup({
+  opts = {
+    buflisted  = true,
+    number     = true,
+    signcolumn = "auto",
+  },
+  highlight = {
+    treesitter   = true,
+    lsp          = false,
+    load_buffers = false,
+  },
+  max_filename_width = function()
+    return math.floor(math.min(45, vim.o.columns / 2))
+  end,
 })
