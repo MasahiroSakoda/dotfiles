72bd119b21d72acf9764dc75c86be8e752fe62cc
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-09 00:01:09 +0900

    feat(neovim): add `Snacks.gh` internal keymaps

--- home/dot_config/nvim/lua/user/snacks/gh.lua
+++ home/dot_config/nvim/lua/user/snacks/gh.lua
@@ -3,4 +3,12 @@
 ---@class snacks.gh.Config
 return {
   enable = true,
+
+  keys = {
+    select  = { "<cr>", "gh_actions", desc = "Select Action" },
+    edit    = { "i"   , "gh_edit"   , desc = "Edit" },
+    comment = { "a"   , "gh_comment", desc = "Add Comment" },
+    close   = { "c"   , "gh_close"  , desc = "Close" },
+    reopen  = { "o"   , "gh_reopen" , desc = "Reopen" },
+  },
 }
