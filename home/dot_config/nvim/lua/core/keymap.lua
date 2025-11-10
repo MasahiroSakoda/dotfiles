aee9c064d02b8821eb1317774b4d80e49a685c2f
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-11 04:25:45 +0900

    feat(neovim): update `Snacks.picker.registers()` keymap

--- home/dot_config/nvim/lua/core/keymap.lua
+++ home/dot_config/nvim/lua/core/keymap.lua
@@ -207,14 +207,14 @@ if not is_vscode then
     { "<Leader>/",  "<CMD>lua Snacks.picker.lines()<CR>",                icon = "󰘤 ", desc = " Grep current buffer" },
 
     -- Vim
-    { "<Leader>fh", "<CMD>lua Snacks.picker.help()<CR>",       icon = " ", desc = " Help" },
-    { "<Leader>fq", "<CMD>lua Snacks.picker.qflist()<CR>",     icon = " ", desc = " Quickfix list" },
-    { "<Leader>fl", "<CMD>lua Snacks.picker.loclist()<CR>",    icon = " ", desc = " Location list" },
-    { "<Leader>fj", "<CMD>lua Snacks.picker.jumps()<CR>",      icon = " ", desc = " Jump list" },
-    { "<Leader>fa", "<CMD>lua Snacks.picker.autocmds()<CR>",   icon = " ", desc = " autocmds list" },
-    { "<Leader>fr", "<CMD>lua Snacks.picker.registers()<CR>",  icon = " ", desc = " Register list" },
-    { "<Leader>fu", "<CMD>lua Snacks.picker.undo()<CR>",       icon = " ", desc = " Undo Tree" },
-    { "<Leader>fH", "<CMD>lua Snacks.picker.highlights()<CR>", icon = " ", desc = " Hilight list" },
+    { "<Leader>fh",  "<CMD>lua Snacks.picker.help()<CR>",       icon = " ", desc = " Help" },
+    { "<Leader>fq",  "<CMD>lua Snacks.picker.qflist()<CR>",     icon = " ", desc = " Quickfix list" },
+    { "<Leader>fl",  "<CMD>lua Snacks.picker.loclist()<CR>",    icon = " ", desc = " Location list" },
+    { "<Leader>fj",  "<CMD>lua Snacks.picker.jumps()<CR>",      icon = " ", desc = " Jump list" },
+    { "<Leader>fa",  "<CMD>lua Snacks.picker.autocmds()<CR>",   icon = " ", desc = " autocmds list" },
+    { "<Leader>f\"", "<CMD>lua Snacks.picker.registers()<CR>",  icon = " ", desc = " Register list" },
+    { "<Leader>fu",  "<CMD>lua Snacks.picker.undo()<CR>",       icon = " ", desc = " Undo Tree" },
+    { "<Leader>fH",  "<CMD>lua Snacks.picker.highlights()<CR>", icon = " ", desc = " Hilight list" },
 
     -- Git
     { "<Leader>gf", "<CMD>lua Snacks.picker.git_files()<CR>",    icon = " ", desc = " Git Files" },
