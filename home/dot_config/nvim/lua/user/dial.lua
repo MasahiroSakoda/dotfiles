16a814850cda1951d3ba2fd62dec01937dcf9d38
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-14 00:51:02 +0900

    feat(neovim): remove `cyclic` option for `dial.nvim`
    which is `true` by default

--- home/dot_config/nvim/lua/user/dial.lua
+++ home/dot_config/nvim/lua/user/dial.lua
@@ -9,7 +9,7 @@ local default_augends = {
   augend.constant.alias.bool,
   augend.constant.alias.Alpha,
   augend.constant.alias.alpha,
-  augend.constant.new({ elements = { "true", "false" }, word = true, cyclic = true, preserve_case = true }),
+  augend.constant.new({ elements = { "true", "false" }, word = true, preserve_case = true }),
   augend.date.alias["%m/%d/%Y"],
   augend.date.alias["%Y-%m-%d"],
   augend.date.alias["%m/%d/%Y"],
@@ -22,11 +22,8 @@ local default_augends = {
   augend.date.new({ pattern = "%B", default_kind = "day", word = true }),
   augend.constant.alias.ja_weekday,
   augend.constant.alias.ja_weekday_full,
-  augend.hexcolor.new({ case = "lower", cyclic = true }),
-  augend.case.new({
-    types = { "camelCase", "snake_case", "PascalCase", "SCREAMING_SNAKE_CASE" },
-    cyclic = true,
-  }),
+  augend.hexcolor.new({ case = "lower" }),
+  augend.case.new({ types = { "camelCase", "snake_case", "PascalCase", "SCREAMING_SNAKE_CASE" } }),
 
   augend.semver.alias.semver,
 
@@ -65,9 +62,9 @@ local default_augends = {
   augend.constant.new({ elements = { "passive", "active" },        word = true, preserve_case = true }),
 
   -- Programming symbols
-  augend.constant.new({ elements = { "&&", "||" }, word = false, cyclic = true }),
-  augend.constant.new({ elements = { "==", "!=" }, word = false, cyclic = true }),
-  augend.constant.new({ elements = { "+=", "-=" }, word = false, cyclic = true }),
+  augend.constant.new({ elements = { "&&", "||" }, word = false }),
+  augend.constant.new({ elements = { "==", "!=" }, word = false }),
+  augend.constant.new({ elements = { "+=", "-=" }, word = false }),
   augend.constant.new({ elements = { "stdin", "stdout", "stderr" }, preserve_case = true }),
   augend.constant.new({ elements = { "trace", "debug", "info", "warn", "error", "fatal" }, word = true }),
 
@@ -75,7 +72,6 @@ local default_augends = {
   augend.constant.new({
     elements = { "ollama", "groq", "copilot", "openai", "anthropic", "google" },
     word = true,
-    cyclic = true,
     preserve_case = true,
   }),
 }
@@ -85,60 +81,60 @@ dial.augends:register_group({ default = default_augends })
 dial.augends:on_filetype({
   lua = vim.list_extend(default_augends, {
     augend.paren.alias.lua_str_literal,
-    augend.constant.new({ elements = { "==", "~=" }, word = false, cyclic = true }),
+    augend.constant.new({ elements = { "==", "~=" }, word = false }),
   }),
 
   markdown = vim.list_extend(default_augends, {
     augend.misc.alias.markdown_header,
-    augend.constant.new({ elements = { "[x]", "[ ]" }, word = false, cyclic = true }),
+    augend.constant.new({ elements = { "[x]", "[ ]" }, word = false }),
   }),
 
   rust = vim.list_extend(default_augends, {
     augend.paren.alias.rust_str_literal,
-    augend.constant.new({ elements = { "let", "const" },                    word = true, cyclic = true }),
-    augend.constant.new({ elements = { "type", "trait", "struct", "enum" }, word = true, cyclic = true }),
-    augend.constant.new({ elements = { "isize", "i8", "i16", "i32", "i64", "i128" }, word = true, cyclic = true }),
-    augend.constant.new({ elements = { "usize", "u8", "u16", "u32", "u64", "u128" }, word = true, cyclic = true }),
-    augend.constant.new({ elements = { "f32", "f64" },                               word = true, cyclic = true }),
+    augend.constant.new({ elements = { "let", "const" },                    word = true }),
+    augend.constant.new({ elements = { "type", "trait", "struct", "enum" }, word = true }),
+    augend.constant.new({ elements = { "isize", "i8", "i16", "i32", "i64", "i128" }, word = true }),
+    augend.constant.new({ elements = { "usize", "u8", "u16", "u32", "u64", "u128" }, word = true }),
+    augend.constant.new({ elements = { "f32", "f64" },                               word = true }),
   }),
 
   zig = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { "var", "const" }, word = true, cyclic = true }),
+    augend.constant.new({ elements = { "var", "const" }, word = true }),
   }),
 
   haskell = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { "==", "/=" }, word = false, cyclic = true }),
+    augend.constant.new({ elements = { "==", "/=" }, word = false }),
   }),
 
   go = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { ":=", "=" },             word = false, cyclic = true }),
-    augend.constant.new({ elements = { "interface", "struct" }, word = true,  cyclic = true }),
+    augend.constant.new({ elements = { ":=", "=" },             word = false }),
+    augend.constant.new({ elements = { "interface", "struct" }, word = true }),
 
-    augend.constant.new({ elements = { "int", "int8", "int16", "int32", "int64" },      word = true,  cyclic = true }),
-    augend.constant.new({ elements = { "uint", "uint8", "uint16", "uint32", "uint64" }, word = true,  cyclic = true }),
-    augend.constant.new({ elements = { "float32", "float64" },                          word = true,  cyclic = true }),
-    augend.constant.new({ elements = { "complex64", "complex128" },                     word = true,  cyclic = true }),
+    augend.constant.new({ elements = { "int", "int8", "int16", "int32", "int64" },      word = true }),
+    augend.constant.new({ elements = { "uint", "uint8", "uint16", "uint32", "uint64" }, word = true }),
+    augend.constant.new({ elements = { "float32", "float64" },                          word = true }),
+    augend.constant.new({ elements = { "complex64", "complex128" },                     word = true }),
   }),
 
   java = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { "private", "public", "protected" }, word = true, cyclic = true }),
-    augend.constant.new({ elements = { "class", "interface", "enum" },     word = true, cyclic = true }),
+    augend.constant.new({ elements = { "private", "public", "protected" }, word = true }),
+    augend.constant.new({ elements = { "class", "interface", "enum" },     word = true }),
   }),
 
   python = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { "True", "False" }, word = true, cyclic = true }),
+    augend.constant.new({ elements = { "True", "False" }, word = true }),
   }),
 
   css = vim.list_extend(default_augends, {
-    augend.constant.new({ elements = { "px", "em", "rem", "%" }, word = false, cyclic = true }),
+    augend.constant.new({ elements = { "px", "em", "rem", "%" }, word = false }),
   }),
 })
 
 local js_augends = vim.list_extend(default_augends, {
-  augend.constant.new({ elements = { "===", "!==" },                     word = false, cyclic = true }),
-  augend.constant.new({ elements = { "of", "in" },                       word = true,  cyclic = true }),
-  augend.constant.new({ elements = { "let", "const" },                   word = true,  cyclic = true }),
-  augend.constant.new({ elements = { "private", "public", "protected" }, word = true,  cyclic = true }),
+  augend.constant.new({ elements = { "===", "!==" },                     word = false }),
+  augend.constant.new({ elements = { "of", "in" },                       word = true }),
+  augend.constant.new({ elements = { "let", "const" },                   word = true }),
+  augend.constant.new({ elements = { "private", "public", "protected" }, word = true }),
 })
 
 vim.iter({
