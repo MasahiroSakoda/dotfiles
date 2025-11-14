b683a66ba7a01daa39c5e560b7a5b1e635dc2c15
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-15 02:09:52 +0900

    feat(neovim): update custom adapters path

--- home/dot_config/nvim/lua/ai/codecompanion/adapters/init.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/adapters/init.lua
@@ -2,12 +2,12 @@
 
 return {
   http = {
-    groq      = require("ai.codecompanion.adapters.groq"),
-    copilot   = require("ai.codecompanion.adapters.copilot"),
-    openai    = require("ai.codecompanion.adapters.openai"),
-    anthropic = require("ai.codecompanion.adapters.anthropic"),
-    gemini    = require("ai.codecompanion.adapters.gemini"),
-    qwen      = require("ai.codecompanion.adapters.qwen"),
-    ollama    = require("ai.codecompanion.adapters.ollama"),
+    groq      = require("ai.codecompanion.adapters.http.groq"),
+    copilot   = require("ai.codecompanion.adapters.http.copilot"),
+    openai    = require("ai.codecompanion.adapters.http.openai"),
+    anthropic = require("ai.codecompanion.adapters.http.anthropic"),
+    gemini    = require("ai.codecompanion.adapters.http.gemini"),
+    qwen      = require("ai.codecompanion.adapters.http.qwen"),
+    ollama    = require("ai.codecompanion.adapters.http.ollama"),
   },
 }
