228ddb1cf9a8c8523d0e5aa310fce1371fa85b4c
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-16 02:13:22 +0900

    chore(neovim): shorten textobject keymap description

--- home/dot_config/nvim/lua/user/treesitter/textobjects.lua
+++ home/dot_config/nvim/lua/user/treesitter/textobjects.lua
@@ -24,47 +24,47 @@ local wk = require("which-key")
 local select  = require("nvim-treesitter-textobjects.select").select_textobject
 wk.add({
   mode = xo,
-  { "ac", function() select("@class.outer",       "textobjects") end, icon = " ", desc = "Select around class" },
-  { "ic", function() select("@class.inner",       "textobjects") end, icon = " ", desc = "Select inside class" },
-  { "af", function() select("@function.outer",    "textobjects") end, icon = " ", desc = "Select around func" },
-  { "if", function() select("@function.inner",    "textobjects") end, icon = " ", desc = "Select inside func" },
-  { "ar", function() select("@return.outer",      "textobjects") end, icon = " ", desc = "Select around return" },
-  { "ir", function() select("@return.inner",      "textobjects") end, icon = " ", desc = "Select inside return" },
-  { "ab", function() select("@block.outer",       "textobjects") end, icon = " ", desc = "Select around block" },
-  { "ib", function() select("@block.inner",       "textobjects") end, icon = " ", desc = "Select inside block" },
-  { "al", function() select("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Select around loop" },
-  { "il", function() select("@loop.inner",        "textobjects") end, icon = "󰅲 ", desc = "Select inside loop" },
-  { "a?", function() select("@conditional.outer", "textobjects") end, icon = " ", desc = "Select around cond" },
-  { "i?", function() select("@conditional.inner", "textobjects") end, icon = " ", desc = "Select inside cond" },
-  { "ap", function() select("@parameter.outer",   "textobjects") end, icon = " ", desc = "Select around param" },
-  { "ip", function() select("@parameter.inner",   "textobjects") end, icon = " ", desc = "Select inside param" },
-  { "as", function() select("@local.scope",       "textobjects") end, icon = " ", desc = "Select local scope" },
+  { "ac", function() select("@class.outer",       "textobjects") end, icon = " ", desc = "Around class" },
+  { "ic", function() select("@class.inner",       "textobjects") end, icon = " ", desc = "Inside class" },
+  { "af", function() select("@function.outer",    "textobjects") end, icon = " ", desc = "Around func" },
+  { "if", function() select("@function.inner",    "textobjects") end, icon = " ", desc = "Inside func" },
+  { "ar", function() select("@return.outer",      "textobjects") end, icon = " ", desc = "Around return" },
+  { "ir", function() select("@return.inner",      "textobjects") end, icon = " ", desc = "Inside return" },
+  { "ab", function() select("@block.outer",       "textobjects") end, icon = " ", desc = "Around block" },
+  { "ib", function() select("@block.inner",       "textobjects") end, icon = " ", desc = "Inside block" },
+  { "al", function() select("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Around loop" },
+  { "il", function() select("@loop.inner",        "textobjects") end, icon = "󰅲 ", desc = "Inside loop" },
+  { "a?", function() select("@conditional.outer", "textobjects") end, icon = " ", desc = "Around cond" },
+  { "i?", function() select("@conditional.inner", "textobjects") end, icon = " ", desc = "Inside cond" },
+  { "ap", function() select("@parameter.outer",   "textobjects") end, icon = " ", desc = "Around param" },
+  { "ip", function() select("@parameter.inner",   "textobjects") end, icon = " ", desc = "Inside param" },
+  { "as", function() select("@local.scope",       "textobjects") end, icon = " ", desc = "Local scope" },
 }, { noremap = true })
 -- Swap
 local swap   = require("nvim-treesitter-textobjects.swap")
 local sp, sn = swap.swap_previous, swap.swap_next
 wk.add({
   mode = "n",
-  { "g<", function() sp("@parameter.inner") end, icon = " ", desc = "Swap prev argument" },
-  { "g>", function() sn("@parameter.inner") end, icon = " ", desc = "Swap next argument" },
+  { "g<", function() sp("@parameter.inner") end, icon = " ", desc = "Prev arg" },
+  { "g>", function() sn("@parameter.inner") end, icon = " ", desc = "Next arg" },
 }, { noremap = true })
 -- Move
 local move = require("nvim-treesitter-textobjects.move")
 local gps, gns = move.goto_previous_start, move.goto_next_start
 wk.add({
   mode = nxo,
-  { "[c", function() gps("@class.outer",       "textobjects") end, icon = " ", desc = "Go to prev class start" },
-  { "[f", function() gps("@function.outer",    "textobjects") end, icon = " ", desc = "Go to prev func start" },
-  { "[l", function() gps("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Go to prev loop start" },
-  { "[?", function() gps("@conditional.outer", "textobjects") end, icon = " ", desc = "Go to prev cond start" },
-  { "[p", function() gps("@parameter.inner",   "textobjects") end, icon = " ", desc = "Go to prev param start" },
-  { "[r", function() gps("@return.inner",      "textobjects") end, icon = " ", desc = "Go to prev return" },
-  { "]c", function() gns("@class.outer",       "textobjects") end, icon = " ", desc = "Go to next class start" },
-  { "]f", function() gns("@function.outer",    "textobjects") end, icon = " ", desc = "Go to next func start" },
-  { "]l", function() gns("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Go to next loop start" },
-  { "]?", function() gns("@conditional.outer", "textobjects") end, icon = " ", desc = "Go to next cond start" },
-  { "]p", function() gns("@parameter.inner",   "textobjects") end, icon = " ", desc = "Go to prev param start" },
-  { "]r", function() gns("@return.inner",      "textobjects") end, icon = " ", desc = "Go to next return" },
+  { "[c", function() gps("@class.outer",       "textobjects") end, icon = " ", desc = "Prev class start" },
+  { "[f", function() gps("@function.outer",    "textobjects") end, icon = " ", desc = "Prev func start" },
+  { "[l", function() gps("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Prev loop start" },
+  { "[?", function() gps("@conditional.outer", "textobjects") end, icon = " ", desc = "Prev cond start" },
+  { "[p", function() gps("@parameter.inner",   "textobjects") end, icon = " ", desc = "Prev param start" },
+  { "[r", function() gps("@return.inner",      "textobjects") end, icon = " ", desc = "Prev return" },
+  { "]c", function() gns("@class.outer",       "textobjects") end, icon = " ", desc = "Next class start" },
+  { "]f", function() gns("@function.outer",    "textobjects") end, icon = " ", desc = "Next func start" },
+  { "]l", function() gns("@loop.outer",        "textobjects") end, icon = "󰅲 ", desc = "Next loop start" },
+  { "]?", function() gns("@conditional.outer", "textobjects") end, icon = " ", desc = "Next cond start" },
+  { "]p", function() gns("@parameter.inner",   "textobjects") end, icon = " ", desc = "Prev param start" },
+  { "]r", function() gns("@return.inner",      "textobjects") end, icon = " ", desc = "Next return" },
 }, { noremap = true })
 -- Repeatable Move
 local repmove = require("nvim-treesitter-textobjects.repeatable_move")
