a21cf7082a0071e0906c1a7456510c0c14e56976
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-11-25 09:59:56 +0900

    fix(neovim): comment out to prevent warning

--- home/dot_config/nvim/lua/snippets/all.lua
+++ home/dot_config/nvim/lua/snippets/all.lua
@@ -2,9 +2,9 @@
 ---@diagnostic disable: unused-local
 local ls = require("luasnip")
 local s  = ls.snippet
-local t  = ls.text_node
+-- local t  = ls.text_node
 local i  = ls.insert_node
-local c  = ls.choice_node
+-- local c  = ls.choice_node
 -- local d  = ls.dynamic_node
 -- local r  = ls.restore_node
 local f  = ls.function_node
