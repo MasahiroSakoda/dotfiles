6424e0909920d16cc55b6d4d49bcd90a3e85b9e0
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-12-13 00:38:25 +0900

    feat(neovim): enable `Snacks.indent` scope option

--- home/dot_config/nvim/lua/user/snacks/indent.lua
+++ home/dot_config/nvim/lua/user/snacks/indent.lua
@@ -21,4 +21,21 @@ return {
       "SnacksIndent8",
     },  ---@type string|string[] hl group for indent guides
   },
+
+  scope = {
+    enabled      = true,
+    priority     = 200,
+    underline    = true,
+    only_current = false,
+    hl = {
+      "SnacksIndent1",
+      "SnacksIndent2",
+      "SnacksIndent3",
+      "SnacksIndent4",
+      "SnacksIndent5",
+      "SnacksIndent6",
+      "SnacksIndent7",
+      "SnacksIndent8",
+    },  ---@type string|string[] hl group for scopes
+  },
 }
