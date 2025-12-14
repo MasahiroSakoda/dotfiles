545dcd8122beb2e7e0bb0f2ab3c5758b6870cb67
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-12-15 02:44:47 +0900

    feat(neovim): specify gemini cli model in `codecompanion.nvim`

--- home/dot_config/nvim/lua/ai/codecompanion/adapters/acp/gemini_cli.lua
+++ home/dot_config/nvim/lua/ai/codecompanion/adapters/acp/gemini_cli.lua
@@ -3,8 +3,15 @@
 return function()
   return require("codecompanion.adapters").extend("gemini_cli", {
     defaults = {
-      auth_method = "oauth-personal", ---@type "oauth-personal"|"gemini-api-key"|"vertex-ai"
+      ---@type "oauth-personal"|"gemini-api-key"|"vertex-ai"
+      auth_method = "oauth-personal",
     },
     env = {},
+    ---@see https://geminicli.com/docs/get-started/configuration/#command-line-arguments
+    commands = {
+      default = { "gemini", "-m", "gemini-2.5-flash" },
+      flash   = { "gemini", "-m", "gemini-2.5-flash" },
+      pro     = { "gemini", "-m", "gemini-2.5-pro" }
+    },
   })
 end
