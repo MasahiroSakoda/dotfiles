707098bdf24457164bc9caebcb2840fffa42cd5e
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-12-20 13:08:18 +0900

    feat(shell): add environment variables related `zellij`

---
+++ home/dot_config/fish/conf.d/zellij.fish
@@ -0,0 +1,5 @@
+# -*-mode:fish-*- vim:ft=fish
+
+set -gx ZELLIJ_CONFIG_DIR "$XDG_CONFIG_HOME/zellij"
+set -gx ZELLIJ_CONFIG_FILE "$ZELLIJ_CONFIG_DIR/config.kdl"
+set -gx ZELLIJ_LAYOUTS_DIR "$ZELLIJ_CONFIG_DIR/layouts"
