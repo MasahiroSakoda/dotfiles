c2eb98964d3392cad035831ba5604fce5e524399
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-01-06 21:05:40 +0900

    feat(wezterm): disable `show_new_tab_button_in_tab_bar` option

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -66,7 +66,7 @@ config.tab_bar_at_bottom = true ---@type boolean
 
 config.show_tabs_in_tab_bar           = true ---@type boolean
 config.show_tab_index_in_tab_bar      = true ---@type boolean
-config.show_new_tab_button_in_tab_bar = true ---@type boolean
+config.show_new_tab_button_in_tab_bar = false---@type boolean
 config.hide_tab_bar_if_only_one_tab   = true ---@type boolean
 
 -- Cursor
