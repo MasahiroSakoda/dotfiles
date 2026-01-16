907778dd8a8ece5a838a62fb66d15621ae49eb93
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-01-16 10:45:35 +0900

    feat(neovim): replace `lazygit` keymap to `gitui`

--- home/dot_config/nvim/lua/core/keymap.lua
+++ home/dot_config/nvim/lua/core/keymap.lua
@@ -360,7 +360,7 @@ wk.add({
   { "[g", "<CMD>Gitsigns nav_hunk prev<CR>", icon = " ", desc = " Jump to prev hunk" },
   { "]g", "<CMD>Gitsigns nav_hunk next<CR>", icon = " ", desc = " Jump to next hunk" },
 
-  { "<Leader>gg", "<CMD>lua Snacks.lazygit()<CR>", mode = nt, icon = " ", desc = " Toggle lazygit" },
+  { "<Leader>gg",  "<CMD>lua Snacks.terminal('gitui')<CR>",   mode = nt, icon = " ", desc = " Toggle GitUI" },
 }, opts)
 
 ---------------------------------------------------------------------------
