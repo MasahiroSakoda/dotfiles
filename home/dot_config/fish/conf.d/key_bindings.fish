bcd5378f0be5bfd2d1bc3d5b68d34f3c6ddabda6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-03-02 00:06:31 +0900

    refactor(fish): consolidate key bindings into single file
    • Merge individual key binding files into `key_bindings.fish`
    • Remove separate configuration files for cleaner `conf.d` structure
    • Update redirection syntax from `2>/dev/null` to `2>&1`

---
+++ home/dot_config/fish/conf.d/key_bindings.fish
@@ -0,0 +1,11 @@
+bind \cg "__ghq_browse_repository"
+if bind --mode insert >/dev/null 2>&1
+  bind --mode insert \cg "__ghq_browse_repository"
+end
+
+bind \cg "__ghq_browse_repository"
+if bind --mode insert >/dev/null 2>&1
+  bind --mode insert \cg "__ghq_browse_repository"
+end
+
+# -*-mode:fish-*- vim:ft=fish
--- home/dot_config/fish/conf.d/nvim_key_bindings.fish
+++
@@ -1,6 +0,0 @@
-# -*-mode:fish-*- vim:ft=fish
-
-bind \cx "__nvim_browse_plugin"
-if bind --mode insert >/dev/null 2>/dev/null
-  bind --mode insert \cx "__nvim_browse_plugin"
-end
