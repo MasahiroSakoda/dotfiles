dc30d9baf5b4c586b7bb74dac8ef6f07a9dabaff
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-03-09 16:25:10 +0900

    refactor(nvim): refine markdown filetype associations
    • Update Marksman LSP configuration to only target "markdown" filetype
    • Register "mdx" as a markdown-flavored language in TreeSitter
    • Remove redundant filetype associations to improve consistency

--- home/dot_config/nvim/lua/lsp/servers/marksman.lua
+++ home/dot_config/nvim/lua/lsp/servers/marksman.lua
@@ -3,7 +3,7 @@
 ---@type vim.lsp.Config
 return {
   cmd          = { "marksman", "server" },
-  filetypes    = { "md", "markdown", "mdx" },
+  filetypes    = { "markdown" },
   root_markers = { ".git" },
   single_file_support = true,
 }
