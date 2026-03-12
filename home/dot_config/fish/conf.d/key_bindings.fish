9c6394f06a87b51ba7581d7da03b6c8c324563b9
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-03-12 09:08:39 +0900

    feat(fish): update key bindings for plugin browsing
    • Replace `\cg` with `\cx` for improved key binding consistency
    
    • Change the bound command from `__ghq_browse_repository` to `__nvim_browse_plugin`
    
    • Update both normal and insert mode key bindings for fish shell

--- home/dot_config/fish/conf.d/key_bindings.fish
+++ home/dot_config/fish/conf.d/key_bindings.fish
@@ -3,9 +3,9 @@ if bind --mode insert >/dev/null 2>&1
   bind --mode insert \cg "__ghq_browse_repository"
 end
 
-bind \cg "__ghq_browse_repository"
+bind \cx "__nvim_browse_plugin"
 if bind --mode insert >/dev/null 2>&1
-  bind --mode insert \cg "__ghq_browse_repository"
+  bind --mode insert \cx "__nvim_browse_plugin"
 end
 
 # -*-mode:fish-*- vim:ft=fish
