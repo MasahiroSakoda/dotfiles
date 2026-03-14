50acb2596ed89018b73a20fa73dcd9ac8fdf53c5
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-03-14 20:12:05 +0900

    feat(shell): add preview page navigation to browser functions
    • Add ctrl-b and ctrl-f keybindings for fzf preview window
    • Update Fish and Zsh repository browser functions for ghq and nvim
    • Improve readability of README previews in repository browsers

--- home/dot_config/zsh/functions/nvim.zsh
+++ home/dot_config/zsh/functions/nvim.zsh
@@ -6,7 +6,7 @@ function _nvim_browse_plugin() {
           --header "List of NeoVim plugins | Enter: Browse | Esc: Exit" \
           --footer "Navigate with Ctrl + h/j/k/l or arrows" \
           --preview 'bat --color=always {}/README.md || echo "No README.md"' \
-          --bind 'ctrl-u:preview-up,ctrl-d:preview-down,ctrl-/:toggle-preview' --exit-0)"
+          --bind 'ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-up,ctrl-d:preview-down,ctrl-/:toggle-preview' --exit-0)"
   [ -n "${repo}" ] && cd "${repo}" && nvim
   zle accept-line
   zle reset-prompt
