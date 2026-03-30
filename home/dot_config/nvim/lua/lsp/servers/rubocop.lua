0f5ef4e341629a0e2f34ad135b69e9c6a9b303c8
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-03-31 08:55:34 +0900

    refactor(nvim): remove dedicated LSP configs for linters and formatters
    • Delete individual LSP server configurations for biome, rubocop, ruff, stylua, and typos
    • Remove linter and formatter servers from the core LSP initialization list
    • Update mise configuration to use aqua:crate-ci/typos instead of typos-lsp

--- home/dot_config/nvim/lua/lsp/servers/rubocop.lua
+++
@@ -1,9 +0,0 @@
--- -*-mode:lua-*- vim:ft=lua
-
----@type vim.lsp.Config
-return {
-  cmd          = { "rubocop", "--lsp" },
-  filetypes    = { "ruby" },
-  root_markers = require("user.filetypes").lsp.ruby,
-  single_file_support = true,
-}
--- home/dot_config/nvim/lua/lsp/servers/stylua.lua
+++
@@ -1,8 +0,0 @@
--- -*-mode:lua-*- vim:ft=lua
-
----@type vim.lsp.Config
-return {
-	cmd = { "stylua", "--lsp", "--search-parent-directories" },
-	filetypes = { "lua" },
-	root_markers = { ".stylua.toml", "stylua.toml", ".editorconfig" },
-}
