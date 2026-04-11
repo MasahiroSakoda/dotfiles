87278c68b5a4d5f8bf778b7af1d3361d8b156e2f
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-04-12 01:56:04 +0900

    chore(nvim): update sqls data source template
    • Update placeholder credentials in sqls configuration

--- home/dot_config/nvim/lua/lsp/servers/sqls.lua
+++ home/dot_config/nvim/lua/lsp/servers/sqls.lua
@@ -12,7 +12,7 @@ return {
         -- },
         -- {
         --   driver = "postgresql",
-        --   dataSourceName = "host=127.0.0.1 port=15432 user=postgres password=mysecretpassword1234 dbname=dvdrental sslmode=disable"
+        --   dataSourceName = "host=127.0.0.1 port=15432 user=postgres password=password dbname=sample sslmode=disable"
         -- },
       },
     },
