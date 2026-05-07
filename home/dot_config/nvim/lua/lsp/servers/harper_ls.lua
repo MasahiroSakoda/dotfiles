89c4884dfd7a481902cdd404f0cde9c2f27d76f3
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-05-08 00:52:58 +0900

    feat(nvim): update harper-ls linter rules
    • Define explicit linter rules in harper_ls.lua
    • Disable word expansion and long sentence rules
    • Add documentation link for Harper linter rules

--- home/dot_config/nvim/lua/lsp/servers/harper_ls.lua
+++ home/dot_config/nvim/lua/lsp/servers/harper_ls.lua
@@ -9,7 +9,31 @@ return {
   settings = {
     ["harper-ls"] = {
       diagnosticSeverity = "hint",
-      linters = { SpellCheck = true, SentenceCapitalization = true },
+      ---@see https://writewithharper.com/docs/rules
+      linters = {
+        SpellCheck                   = true,
+        Spaces                       = false,
+        LongSentences                = false,
+        ExpandAlloc                  = false,
+        ExpandArgument               = false,
+        ExpandControl                = false,
+        ExpandDependencies           = false,
+        ExpandForward                = false,
+        ExpandMemoryShorthands       = false,
+        ExpandMinimum                = false,
+        ExpandParameter              = false,
+        ExpandPointer                = false,
+        ExpandPrevious               = false,
+        ExpandStandardInputAndOutput = false,
+        ExpandTimeShorthands         = false,
+        ExpandThrough                = false,
+        ExpandWith                   = false,
+        ExpandWithout                = false,
+        PhrasalVerbAsCompoundNoun    = false,
+        SplitWords                   = false,
+        ToDoHyphen                   = false,
+        SentenceCapitalization       = true
+      },
       markdown = { IgnoreLinkTitle = false },
       dialect = "American",
       maxFileLength = 120000,
