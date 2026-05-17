efd5f96df885eb8418cec4c1cd84a19c2913fef8
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-05-18 03:03:33 +0900

    refactor(lsp): disable automatic lsp formatting
    • Comment out the vim.lsp.buf.format call in formatting configuration
    • Disable the default LSP-based formatting filter logic
    • Allow for manual formatting or alternative formatting methods

--- home/dot_config/nvim/lua/lsp/config/format.lua
+++ home/dot_config/nvim/lua/lsp/config/format.lua
@@ -1,10 +1,10 @@
 -- -*-mode:lua-*- vim:ft=lua
 return function()
-  vim.lsp.buf.format({
-    async = false,
-    timeout_ms = 2000,
-    filter = function(c)
-      return not vim.tbl_contains(require("lsp.config.ignore").format, c.name)
-    end,
-  })
+  -- vim.lsp.buf.format({
+  --   async = false,
+  --   timeout_ms = 2000,
+  --   filter = function(c)
+  --     return not vim.tbl_contains(require("lsp.config.ignore").format, c.name)
+  --   end,
+  -- })
 end
