739169b9cf21c792281646fd06364ce130b550ae
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-07-25 03:51:48 +0900

    style(nvim): comment out unused luasnip imports
    • Comment out unused function_node and fmt imports
    • Clean up dead code in TypeScript React snippets file

--- home/dot_config/nvim/lua/snippets/typescriptreact.lua
+++ home/dot_config/nvim/lua/snippets/typescriptreact.lua
@@ -6,14 +6,14 @@ local i  = ls.insert_node
 -- local c  = ls.choice_node
 -- local d  = ls.dynamic_node
 -- local r  = ls.restore_node
-local f  = ls.function_node
+-- local f  = ls.function_node
 -- local sn = ls.snippet_node
 -- local as = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
 -- local extras = require("luasnip.extras")
 -- local rep    = extras.rep
 -- local m      = extras.m
 -- local l      = extras.l
-local fmt     = require("luasnip.extras.fmt").fmt
+-- local fmt     = require("luasnip.extras.fmt").fmt
 local fmta    = require("luasnip.extras.fmt").fmta
 -- local postfix = require("luasnip.extras.postfix").postfix
 -- local conds   = require("luasnip.extras.conditions")
