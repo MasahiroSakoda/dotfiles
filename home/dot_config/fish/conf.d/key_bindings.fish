36c1de07417db6d23a828c10b08901a5ec14db86
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-08-15 14:56:26 +0900

    refactor(shell): remove ghq and nvim browse keybindings
    • Drop \cg ghq repository browse bindings
    • Drop \cx nvim plugin browse bindings

--- home/dot_config/fish/conf.d/key_bindings.fish
+++ home/dot_config/fish/conf.d/key_bindings.fish
@@ -1,11 +1,2 @@
-bind \cg "__ghq_browse_repository"
-if bind --mode insert >/dev/null 2>&1
-  bind --mode insert \cg "__ghq_browse_repository"
-end
-
-bind \cx "__nvim_browse_plugin"
-if bind --mode insert >/dev/null 2>&1
-  bind --mode insert \cx "__nvim_browse_plugin"
-end
 
 # -*-mode:fish-*- vim:ft=fish
