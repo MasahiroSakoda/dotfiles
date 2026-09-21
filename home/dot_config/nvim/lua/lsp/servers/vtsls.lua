1b60e7afbdd17df75e3007470aecb10b093444df
MasahiroSakoda <thunderbird.m.s@gmail.com>
2026-09-22 01:08:59 +0900

    refactor(nvim): disable vtsls formatting in favor of oxfmt
    • Add on_attach handler to disable vtsls document formatting capabilities
    • Delegate formatting to oxfmt for JavaScript/TypeScript files
    • Fix code style: consistent spacing around assignments

--- home/dot_config/nvim/lua/lsp/servers/vtsls.lua
+++ home/dot_config/nvim/lua/lsp/servers/vtsls.lua
@@ -1,8 +1,8 @@
 ---@type vim.lsp.Config
 return {
-  cmd      = { "vtsls", "--stdio" },
-  filetypes= { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  root_dir = function(bufnr, on_dir)
+  cmd       = { "vtsls", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_dir  = function(bufnr, on_dir)
     local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lock" }
     root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
       or vim.list_extend(root_markers, { ".git" })
@@ -12,6 +12,12 @@ return {
       return
     end
     on_dir(project_root or vim.fn.getcwd())
+  end,
+
+  on_attach = function(client, _)
+    -- Let oxfmt handle the formatting instead of vtsls
+    client.server_capabilities.documentFormattingProvider      = false
+    client.server_capabilities.documentRangeFormattingProvider = false
   end,
 
   single_file_support = true,
